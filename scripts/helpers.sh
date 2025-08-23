5c7119fabef68d6c34ccce82a7fa3b63756ae61e
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-08-23 12:54:13 -0700

    Add question and answer related functions

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -18,6 +18,7 @@ COLOR_MAGENTA="${ESC_SEQ}35;01m"
 COLOR_CYAN="${ESC_SEQ}36;01m"
 COLOR_WHITE="${ESC_SEQ}37;01m"
 
+COLOR_QUESTION="$COLOR_MAGENTA"
 COLOR_REPORT="$COLOR_BLUE"
 COLOR_ADJUST_SETTING="$COLOR_CYAN"
 COLOR_ACTION_TAKEN="$COLOR_GREEN"
@@ -28,6 +29,7 @@ COLOR_KILLED="$COLOR_RED"
 
 SYMBOL_SUCCESS="✅ "
 SYMBOL_FAILURE="❌ "
+SYMBOL_QUESTION="❓ "
 SYMBOL_ADJUST_SETTING="⚙️  "
 SYMBOL_KILLED="☠️ "
 SYMBOL_ACTION_TAKEN="🪚 "
@@ -58,6 +60,75 @@ function success_or_not() {
   else
     printf "\n${SYMBOL_FAILURE}\n"
   fi
+}
+
+function ask_question() {
+  # Output supplied line of text in distinctive color (COLOR_QUESTION), prefixed by SYMBOL_QUESTION
+  printf "%b%s%s%b\n" "$COLOR_QUESTION" "$SYMBOL_QUESTION" "$1" "$COLOR_RESET"
+}
+
+function get_nonblank_answer_to_question() {
+  # Output supplied line of text in distinctive color (COLOR_QUESTION), prefixed by SYMBOL_QUESTION,
+  # prompt user for response, iterating until user provides a nonblank response.
+  #
+  # Usage example: name=$(get_nonblank_answer_to_question "What should the diff be named?")
+  local prompt="$1"
+  local answer
+
+  while true; do
+    ask_question "$prompt"
+    read "answer?→ "
+    [[ -n "${answer// }" ]] && break
+  done
+
+  echo "$answer"
+}
+
+function get_confirmed_answer_to_question() {
+  # Output supplied line of text in distinctive color (COLOR_QUESTION), prefixed by SYMBOL_QUESTION,
+  # prompt user for response, ask user to confirm, and iterate until user provides an affirmative confirmation.
+  #
+  # Usage example: folder=$(get_confirmed_answer_to_question "Where should I save the results?")
+  local prompt="$1"
+  local answer confirm
+
+  while true; do
+    ask_question "$prompt"
+    read "answer?→ "
+    [[ -z "${answer// }" ]] && continue
+
+    ask_question "You entered: '$answer'. Is this correct? (y/n)"
+    read "confirm?→ "
+    case "$confirm" in
+      [Yy]*) break ;;
+    esac
+  done
+
+  echo "$answer"
+}
+
+function get_yes_no_answer_to_question() {
+  # Output supplied line of text in distinctive color (COLOR_QUESTION), prefixed by SYMBOL_QUESTION,
+  # prompt user for response, iterating until user provides either a yes or no equivalent.
+  #
+  # Usage example: 
+  #     if get_yes_no_answer_to_question "Do you want to continue?"; then
+  #       echo "✅ Proceeding"
+  #     else
+  #       echo "❌ Aborted"
+  #     fi
+  
+  local prompt="$1"
+  local response
+
+  while true; do
+    ask_question "$prompt (y/n)"
+    read "response?→ "
+    case "${response:l}" in  # `:l` lowercases in Zsh
+      y|yes) echo "yes"; return 0 ;;
+      n|no)  echo "no";  return 1 ;;
+    esac
+  done
 }
 
 function report() {
