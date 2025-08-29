3d97e156cb4795886fe966eec176717dbc509877
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-08-28 21:26:54 -0700

    Update set_safari_settings.sh

--- scripts/prefs_scripts/set_safari_settings.sh
+++ scripts/prefs_scripts/set_safari_settings.sh
@@ -25,7 +25,7 @@ defaults write com.apple.Safari EnableNarrowTabs -bool false ; success_or_not
 report_adjust_setting "Never automatically open a website in a tab rather than a window"
 defaults write com.apple.Safari TabCreationPolicy -int 0 ; success_or_not
 
-report_adjust_setting "Do NOT navigate tabs with ⌘1 – ⌘9
+report_adjust_setting "Do NOT navigate tabs with ⌘1 – ⌘9"
 defaults write com.apple.Safari Command1Through9SwitchesTabs -bool false ; success_or_not
 
 report_adjust_setting "Show full website address in Smart Search field"
@@ -34,8 +34,15 @@ defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true ; succe
 report_adjust_setting "Always show tab bar"
 defaults write com.apple.Safari AlwaysShowTabBar -bool true ; success_or_not
 
-# report_adjust_setting "Show favorites bar (NOT WORKING)"
-# WARNING: This is not working reliably
+report_adjust_setting "Show favorites bar"
+# Notes:
+# - Each window can independently have its favorites bar shown/hidden
+# - Each time the View » Show/Hide Favorites Bar menu item is chosen, "ShowFavoritesBar-v2" is updated
+# - The current state of "ShowFavoritesBar-v2" dictates whether a *new* window will have its
+#   favorites bar shown or hidden
+# - Thus, the state of "ShowFavoritesBar-v2" in a script such as this establishes the default for the
+#   first browser window opened. As long as the user doesn’t retract that setting, all windows will
+#   show the favorites bar.
 defaults write com.apple.Safari "ShowFavoritesBar-v2" -bool true ; success_or_not
 
 report_adjust_setting "Show status bar"
