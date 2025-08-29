7f18d96bfe173b9bfdf96a9f3e059e909e4bac06
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-08-28 17:56:27 -0700

    Update based on defaults-detective; remove sudo's experimentally

--- scripts/prefs_scripts/set_safari_settings.sh
+++ scripts/prefs_scripts/set_safari_settings.sh
@@ -17,65 +17,39 @@ report_start_phase_standard
 report_action_taken "Implement Safari settings"
 
 report_adjust_setting "Do NOT auto-open “safe” downloads"
-sudo defaults write com.apple.Safari AutoOpenSafeDownloads -bool false;success_or_not
+#sudo defaults write com.apple.Safari AutoOpenSafeDownloads -bool false;success_or_not
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false;success_or_not
+
+report_adjust_setting "Always show website titles in tabs"
+# sudo defaults write com.apple.Safari EnableNarrowTabs -bool false;success_or_not
+defaults write com.apple.Safari EnableNarrowTabs -bool false;success_or_not
 
 report_adjust_setting "Never automatically open a website in a tab rather than a window"
-sudo defaults write com.apple.Safari TabCreationPolicy -int 0;success_or_not
+# sudo defaults write com.apple.Safari TabCreationPolicy -int 0;success_or_not
+defaults write com.apple.Safari TabCreationPolicy -int 0;success_or_not
+
+report_adjust_setting "Do NOT navigate tabs with ⌘1 – ⌘9
+defaults write com.apple.Safari Command1Through9SwitchesTabs -bool false
 
 report_adjust_setting "Show full website address in Smart Search field"
-sudo defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true;success_or_not
+# sudo defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true;success_or_not
+defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true;success_or_not
 
-report_action_taken "Implement “Press Tab to highlight each item on a webpage”"
-report_adjust_setting "1 of 2: WebKitPreferences.tabFocusesLinks"
-sudo defaults write com.apple.Safari WebKitPreferences.tabFocusesLinks -bool true;success_or_not
-report_adjust_setting "2 of 2: WebKitTabToLinksPreferenceKey"
-sudo defaults write com.apple.Safari WebKitTabToLinksPreferenceKey -bool true;success_or_not
-
-report_adjust_setting "Show status bar"
-sudo defaults write com.apple.Safari ShowOverlayStatusBar -bool true;success_or_not
+report_adjust_setting "Always show tab bar"
+defaults write com.apple.Safari AlwaysShowTabBar -bool true
 
 # report_adjust_setting "Show favorites bar (NOT WORKING)"
 # WARNING: This is not working reliably
-# defaults write com.apple.Safari "ShowFavoritesBar-v2" -bool true;success_or_not
-# defaults write com.apple.Safari "ShowFavoritesBar" -bool true;success_or_not
+defaults write com.apple.Safari "ShowFavoritesBar-v2" -bool true;success_or_not
 
-report_adjust_setting "⌘-click opens a link in a new tab (reinforces default)"
-sudo defaults write com.apple.Safari CommandClickMakesTabs -bool true;success_or_not
+report_adjust_setting "Show status bar"
+# sudo defaults write com.apple.Safari ShowOverlayStatusBar -bool true;success_or_not
+defaults write com.apple.Safari ShowOverlayStatusBar -bool true;success_or_not
 
-report_adjust_setting "Do NOT make a new tab/window active"
-sudo defaults write com.apple.Safari OpenNewTabsInFront -bool false;success_or_not
 
-report_adjust_setting "Always show website titles in tabs"
-sudo defaults write com.apple.Safari EnableNarrowTabs -bool false;success_or_not
 
-report_action_taken "Turn on: Prevent cross-site tracking (reinforces default)"
-report_adjust_setting "1 of 3: BlockStoragePolicy"
-sudo defaults write com.apple.Safari BlockStoragePolicy -int 2;success_or_not
-report_adjust_setting "2 of 3: WebKitPreferences.storageBlockingPolicy"
-sudo defaults write com.apple.Safari WebKitPreferences.storageBlockingPolicy -int 1;success_or_not
-report_adjust_setting "3 of 3: WebKitStorageBlockingPolicy"
-sudo defaults write com.apple.Safari WebKitStorageBlockingPolicy -int 1;success_or_not
 
-report_action_taken "Warn if visit a fraudulent site"
-report_adjust_setting "1 of 2: WarnAboutFraudulentWebsites"
-sudo defaults write com.apple.Safari WarnAboutFraudulentWebsites -bool true;success_or_not
-report_adjust_setting "2 of 2: com.apple.Safari.SafeBrowsing » SafeBrowsingEnabled"
-sudo defaults write com.apple.Safari.SafeBrowsing SafeBrowsingEnabled -bool true;success_or_not
 
-report_action_taken "Show features for web developers"
-report_adjust_setting "1 of 5: IncludeDevelopMenu"
-sudo defaults write com.apple.Safari IncludeDevelopMenu -bool true;success_or_not
-report_adjust_setting "2 of 5: MobileDeviceRemoteXPCEnabled"
-sudo defaults write com.apple.Safari MobileDeviceRemoteXPCEnabled -bool true;success_or_not
-report_adjust_setting "3 of 5: WebKitDeveloperExtrasEnabledPreferenceKey"
-sudo defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true;success_or_not
-report_adjust_setting "4 of 5: WebKitPreferences.developerExtrasEnabled"
-sudo defaults write com.apple.Safari WebKitPreferences.developerExtrasEnabled -bool true;success_or_not
-report_adjust_setting "5 of 5: Safari.SandboxBroker: IncludeDevelopMenu"
-sudo defaults write com.apple.Safari.SandboxBroker ShowDevelopMenu -bool true;success_or_not
-
-report_adjust_setting "Reveal internal debug menu"
-sudo defaults write com.apple.Safari IncludeInternalDebugMenu -bool true;success_or_not
 
 report_end_phase_standard
 
