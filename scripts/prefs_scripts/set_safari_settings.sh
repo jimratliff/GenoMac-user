828edef3958f5a5544c0950cc7a53cf790a75315
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-08-28 18:05:27 -0700

    Add success_or_not

--- scripts/prefs_scripts/set_safari_settings.sh
+++ scripts/prefs_scripts/set_safari_settings.sh
@@ -17,34 +17,30 @@ report_start_phase_standard
 report_action_taken "Implement Safari settings"
 
 report_adjust_setting "Do NOT auto-open “safe” downloads"
-#sudo defaults write com.apple.Safari AutoOpenSafeDownloads -bool false;success_or_not
-defaults write com.apple.Safari AutoOpenSafeDownloads -bool false;success_or_not
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false ; success_or_not
 
 report_adjust_setting "Always show website titles in tabs"
-# sudo defaults write com.apple.Safari EnableNarrowTabs -bool false;success_or_not
-defaults write com.apple.Safari EnableNarrowTabs -bool false;success_or_not
+defaults write com.apple.Safari EnableNarrowTabs -bool false ; success_or_not
 
 report_adjust_setting "Never automatically open a website in a tab rather than a window"
-# sudo defaults write com.apple.Safari TabCreationPolicy -int 0;success_or_not
-defaults write com.apple.Safari TabCreationPolicy -int 0;success_or_not
+defaults write com.apple.Safari TabCreationPolicy -int 0 ; success_or_not
 
 report_adjust_setting "Do NOT navigate tabs with ⌘1 – ⌘9
 defaults write com.apple.Safari Command1Through9SwitchesTabs -bool false ; success_or_not
 
 report_adjust_setting "Show full website address in Smart Search field"
-# sudo defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true;success_or_not
-defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true;success_or_not
+defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true ; success_or_not
 
 report_adjust_setting "Always show tab bar"
-defaults write com.apple.Safari AlwaysShowTabBar -bool true
+defaults write com.apple.Safari AlwaysShowTabBar -bool true ; success_or_not
 
 # report_adjust_setting "Show favorites bar (NOT WORKING)"
 # WARNING: This is not working reliably
-defaults write com.apple.Safari "ShowFavoritesBar-v2" -bool true;success_or_not
+defaults write com.apple.Safari "ShowFavoritesBar-v2" -bool true ; success_or_not
 
 report_adjust_setting "Show status bar"
-# sudo defaults write com.apple.Safari ShowOverlayStatusBar -bool true;success_or_not
-defaults write com.apple.Safari ShowOverlayStatusBar -bool true;success_or_not
+# sudo defaults write com.apple.Safari ShowOverlayStatusBar -bool true ; success_or_not
+defaults write com.apple.Safari ShowOverlayStatusBar -bool true ; success_or_not
 
 
 
