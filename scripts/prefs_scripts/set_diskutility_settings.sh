e4bca56d3e7e3b4173499b8fb7132f53444625fe
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-06 13:28:51 -0700

    Remove launch-and-quit (moved to other script)

--- scripts/prefs_scripts/set_diskutility_settings.sh
+++ scripts/prefs_scripts/set_diskutility_settings.sh
@@ -16,10 +16,10 @@ function set_diskutility_settings() {
 report_start_phase_standard
 
 # Launch and quit DiskUtility in order that it will have preferences to modify.
-report_action_taken "Launch and quit DiskUtility in order that it will have preferences to modify"
-open -b com.apple.DiskUtility # By bundle ID (more reliable than `open -a` by display name)
-sleep 2
-osascript -e 'quit app "Disk Utility"';success_or_not
+# report_action_taken "Launch and quit DiskUtility in order that it will have preferences to modify"
+# open -b com.apple.DiskUtility # By bundle ID (more reliable than `open -a` by display name)
+# sleep 2
+# osascript -e 'quit app "Disk Utility"';success_or_not
 
 # DiskUtility: Show all devices in sidebar
 report_adjust_setting "DiskUtility: Show all devices in sidebar"
