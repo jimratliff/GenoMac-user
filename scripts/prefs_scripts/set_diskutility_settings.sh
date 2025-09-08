dc9b144adc9464d6c5f54f44ed3c702ecc208ec7
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-07 18:52:42 -0700

    Update set_diskutility_settings.sh

--- scripts/prefs_scripts/set_diskutility_settings.sh
+++ scripts/prefs_scripts/set_diskutility_settings.sh
@@ -19,12 +19,6 @@ domain="com.apple.DiskUtility"
 
 ensure_plist_exists "${domain}"
 
-# Launch and quit DiskUtility in order that it will have preferences to modify.
-# report_action_taken "Launch and quit DiskUtility in order that it will have preferences to modify"
-# open -b com.apple.DiskUtility # By bundle ID (more reliable than `open -a` by display name)
-# sleep 2
-# osascript -e 'quit app "Disk Utility"';success_or_not
-
 # DiskUtility: Show all devices in sidebar
 report_adjust_setting "DiskUtility: Show all devices in sidebar"
 defaults write ${domain} SidebarShowAllDevices -bool true;success_or_not
