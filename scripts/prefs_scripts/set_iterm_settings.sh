290e22b06ac4b905e571a09ad125d49b316833b1
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-07 21:57:09 -0700

    Fix newline in warning message

--- scripts/prefs_scripts/set_iterm_settings.sh
+++ scripts/prefs_scripts/set_iterm_settings.sh
@@ -38,7 +38,7 @@ report_adjust_setting "Change default font to Fira Code Nerd Font"
 # iTerm’s plist is not sufficiently populated to support setting the default font unless iTerm has been launched once.
 # We check whether there is a sufficiently populated plist. If not, launch iTerm to create that plist.
 if ! "${PLISTBUDDY_PATH}" -c 'Print :"New Bookmarks":0:"Normal Font"' "${plist_path}" >/dev/null 2>&1; then
-    report_warning "iTerm2 preferences not properly initialized, launching iTerm2 to properly populate plist file."
+    report_warning $'\niTerm2 preferences not properly initialized, launching iTerm2 to properly populate plist file.'
     launch_and_quit_app "${domain}"
 fi
 
