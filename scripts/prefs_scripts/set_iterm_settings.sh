d201f83091a5abf3d986e44855745eaeaaae0328
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-07 21:29:21 -0700

    Update set_iterm_settings.sh

--- scripts/prefs_scripts/set_iterm_settings.sh
+++ scripts/prefs_scripts/set_iterm_settings.sh
@@ -1,6 +1,8 @@
-# This file assumes GENOMAC_HELPER_DIR is already set in the current shell
-# to the absolute path of the directory containing helpers.sh.
-# That variable must be defined before this file is sourced.
+# This file assumes:
+# - GENOMAC_HELPER_DIR is already set in the current shell to the absolute path of the directory 
+#   containing helpers.sh.
+# - PLISTBUDDY_PATH
+# These environment variables must be defined by assign_environment_variables.sh
 
 if [[ -z "${GENOMAC_HELPER_DIR:-}" ]]; then
   echo "❌ GENOMAC_HELPER_DIR is not set. Please source `initial_prefs.sh` first."
@@ -16,6 +18,14 @@ function set_iterm_settings() {
 report_start_phase_standard
 report_action_taken "Implement iTerm2 settings"
 
+domain="com.googlecode.iterm2"
+
+# It is insufficient to test for the existence of the plist file. The plist file can exist but yet
+# not contain the entry needed to set the default font.
+# ensure_plist_exists "${domain}"
+
+local plist_path=$(plist_path_from_domain "$domain")
+
 # Launch and quit iTerm2 in order that it will have preferences to modify.
 # report_action_taken "Launch and quit iTerm2 in order that it will have preferences to modify"
 # open -b com.googlecode.iterm2 # By bundle ID (more reliable than `open -a` by display name)
@@ -23,16 +33,26 @@ report_action_taken "Implement iTerm2 settings"
 # osascript -e 'quit app "iTerm2"';success_or_not
 
 report_adjust_setting "Set: Open windows in same Spaces"
-defaults write com.googlecode.iterm2 RestoreWindowsToSameSpaces -bool true ; success_or_not
+defaults write ${domain} RestoreWindowsToSameSpaces -bool true ; success_or_not
 
 report_adjust_setting "Set Theme to Dark"
 # The default value, Regular, corresponds to 1
-defaults write com.googlecode.iterm2 TabStyleWithAutomaticOption -int 1 ; success_or_not
+defaults write ${domain} TabStyleWithAutomaticOption -int 1 ; success_or_not
 
 report_adjust_setting "Change default font to Fira Code Nerd Font"
+# report "PLISTBUDDY_PATH is ${PLISTBUDDY_PATH}"
+# report "plist_path is ${plist_path}"
+
+#"${PLISTBUDDY_PATH}" -c 'Set :"New Bookmarks":0:"Normal Font" "FiraCodeNFM-Reg 12"' "${plist_path}"
+# success_or_not
+
 /usr/libexec/PlistBuddy \
   -c 'Set :"New Bookmarks":0:"Normal Font" "FiraCodeNFM-Reg 12"' \
   ~/Library/Preferences/com.googlecode.iterm2.plist ; success_or_not
+
+#/usr/libexec/PlistBuddy \
+#  -c 'Set :"New Bookmarks":0:"Normal Font" "FiraCodeNFM-Reg 12"' "${plist_path}" ; success_or_not
+#  ~/Library/Preferences/com.googlecode.iterm2.plist ; success_or_not
 
 report_end_phase_standard
 
