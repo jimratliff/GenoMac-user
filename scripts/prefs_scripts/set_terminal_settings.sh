c1f6285a9b1091adc3543be6835bbd9550612042
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-10 16:06:37 -0700

    Update set_terminal_settings.sh

--- scripts/prefs_scripts/set_terminal_settings.sh
+++ scripts/prefs_scripts/set_terminal_settings.sh
@@ -15,11 +15,13 @@ function set_terminal_settings() {
 
 report_start_phase_standard
 
+local domain="com.apple.Terminal"
+
 report_action_taken "Give the Terminal a teeny bit of style, even though we will soon abandon it"
 report_adjust_setting "Terminal: default for new windows: “Man Page”";success_or_not
-defaults write com.apple.Terminal "Default Window Settings" -string "Man Page"
+defaults write "${domain}" "Default Window Settings" -string "Man Page"
 report_adjust_setting "Terminal: default for starting windows: “Man Page”";success_or_not
-defaults write com.apple.Terminal "Startup Window Settings" -string "Man Page"
+defaults write "${domain}" "Startup Window Settings" -string "Man Page"
 
 report_end_phase_standard
 
