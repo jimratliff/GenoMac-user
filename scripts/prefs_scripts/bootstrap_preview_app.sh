44e15589204d7a843c567711c61c70a0980fede5
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-11 18:28:06 -0700

    Update bootstrap_preview_app.sh

--- scripts/prefs_scripts/bootstrap_preview_app.sh
+++ scripts/prefs_scripts/bootstrap_preview_app.sh
@@ -45,6 +45,9 @@ report_action_taken "Reconfigure Toolbar"
 "$PLISTBUDDY_PATH" -c "Add '$toolbar_key:TB Item Identifiers:2' string previous_next" "$plist_path"
 "$PLISTBUDDY_PATH" -c "Add '$toolbar_key:TB Item Identifiers:3' string search" "$plist_path"
 
+report_action_taken "Killing Preview (metaphorically)."
+killall Preview ; success_or_not
+
 report_end_phase_standard
 
 }
