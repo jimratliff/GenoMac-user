c6020a32606d812031bbc7561a672a7c207aa8c9
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-16 14:35:43 -0700

    Update assign_environment_variables.sh

--- scripts/assign_environment_variables.sh
+++ scripts/assign_environment_variables.sh
@@ -51,6 +51,12 @@ source "${GENOMAC_HELPER_DIR}/helpers.sh"
 # Specify location of PlistBuddy
 PLISTBUDDY_PATH='/usr/libexec/PlistBuddy'
 
+# Establish symbolic names for modifier keys
+SHIFT_CHAR=$'\u21e7'     # ⇧
+CONTROL_CHAR=$'\u2303'   # ⌃  
+OPTION_CHAR=$'\u2325'    # ⌥
+COMMAND_CHAR=$'\u2318'   # ⌘
+
 # Specify URL for cloning the public GenoMac-system repository using HTTPS
 GENOMAC_SYSTEM_REPO_URL="https://github.com/jimratliff/GenoMac-system.git"
 
@@ -90,6 +96,10 @@ function export_and_report() {
 }
 
 export_and_report PLISTBUDDY_PATH
+export_and_report SHIFT_CHAR
+export_and_report CONTROL_CHAR
+export_and_report OPTION_CHAR
+export_and_report COMMAND_CHAR
 export_and_report HOMEBREW_PREFIX
 export_and_report GENOMAC_HELPER_DIR
 export_and_report GENOMAC_SYSTEM_REPO_URL
