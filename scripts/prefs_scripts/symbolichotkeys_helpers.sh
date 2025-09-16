76c712685c8b914acad43615456a75b4e8dc9fc2
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-15 19:59:04 -0700

    Add get_hotkey_ascii_and_AppleScript_key_codes()

--- scripts/prefs_scripts/symbolichotkeys_helpers.sh
+++ scripts/prefs_scripts/symbolichotkeys_helpers.sh
@@ -20,6 +20,166 @@ domain="com.apple.symbolichotkeys"
 symdict="AppleSymbolicHotKeys"
 
 
+function get_hotkey_ascii_and_AppleScript_key_codes() {
+  # Function to map key descriptions to ASCII code and AppleScript key code
+  # Returns: "ascii_code applescript_code" or "ERROR" if key not found
+    local key="$1"
+    
+    # Check if argument is provided
+    if [[ -z "$key" ]]; then
+        echo "Error: get_key_codes requires a key description argument" >&2
+        return 1
+    fi
+    
+    # Convert to lowercase for easier matching
+    local key_lower="${key:l}"
+    
+    # Define associative arrays for the mappings
+    local -A ascii_codes applescript_codes
+    
+    # Letters (lowercase and uppercase map to same AppleScript codes)
+    ascii_codes[a]=97;   applescript_codes[a]=0
+    ascii_codes[b]=98;   applescript_codes[b]=11
+    ascii_codes[c]=99;   applescript_codes[c]=8
+    ascii_codes[d]=100;  applescript_codes[d]=2
+    ascii_codes[e]=101;  applescript_codes[e]=14
+    ascii_codes[f]=102;  applescript_codes[f]=3
+    ascii_codes[g]=103;  applescript_codes[g]=5
+    ascii_codes[h]=104;  applescript_codes[h]=4
+    ascii_codes[i]=105;  applescript_codes[i]=34
+    ascii_codes[j]=106;  applescript_codes[j]=38
+    ascii_codes[k]=107;  applescript_codes[k]=40
+    ascii_codes[l]=108;  applescript_codes[l]=37
+    ascii_codes[m]=109;  applescript_codes[m]=46
+    ascii_codes[n]=110;  applescript_codes[n]=45
+    ascii_codes[o]=111;  applescript_codes[o]=31
+    ascii_codes[p]=112;  applescript_codes[p]=35
+    ascii_codes[q]=113;  applescript_codes[q]=12
+    ascii_codes[r]=114;  applescript_codes[r]=15
+    ascii_codes[s]=115;  applescript_codes[s]=1
+    ascii_codes[t]=116;  applescript_codes[t]=17
+    ascii_codes[u]=117;  applescript_codes[u]=32
+    ascii_codes[v]=118;  applescript_codes[v]=9
+    ascii_codes[w]=119;  applescript_codes[w]=13
+    ascii_codes[x]=120;  applescript_codes[x]=7
+    ascii_codes[y]=121;  applescript_codes[y]=16
+    ascii_codes[z]=122;  applescript_codes[z]=6
+    
+    # Numbers
+    ascii_codes[0]=48;   applescript_codes[0]=29
+    ascii_codes[1]=49;   applescript_codes[1]=18
+    ascii_codes[2]=50;   applescript_codes[2]=19
+    ascii_codes[3]=51;   applescript_codes[3]=20
+    ascii_codes[4]=52;   applescript_codes[4]=21
+    ascii_codes[5]=53;   applescript_codes[5]=23
+    ascii_codes[6]=54;   applescript_codes[6]=22
+    ascii_codes[7]=55;   applescript_codes[7]=26
+    ascii_codes[8]=56;   applescript_codes[8]=28
+    ascii_codes[9]=57;   applescript_codes[9]=25
+    
+    # Special characters (using their ASCII values)
+    ascii_codes['~']=126; applescript_codes['~']=50
+    ascii_codes['!']=33;  applescript_codes['!']=18
+    ascii_codes['@']=64;  applescript_codes['@']=19
+    ascii_codes['#']=35;  applescript_codes['#']=20
+    ascii_codes['$']=36;  applescript_codes['$']=21
+    ascii_codes['%']=37;  applescript_codes['%']=23
+    ascii_codes['^']=94;  applescript_codes['^']=22
+    ascii_codes['&']=38;  applescript_codes['&']=26
+    ascii_codes['*']=42;  applescript_codes['*']=28
+    ascii_codes['(']=40;  applescript_codes['(']=25
+    ascii_codes[')']=41;  applescript_codes[')']=29
+    ascii_codes['-']=45;  applescript_codes['-']=27
+    ascii_codes['+']=43;  applescript_codes['+']=24
+    ascii_codes['=']=61;  applescript_codes['=']=24
+    ascii_codes['[']=91;  applescript_codes['[']=33
+    ascii_codes[']']=93;  applescript_codes[']']=30
+    ascii_codes['\']=92; applescript_codes['\']=42
+    ascii_codes[';']=59;  applescript_codes[';']=41
+    ascii_codes["'"]=39;  applescript_codes["'"]=39
+    ascii_codes[',']=44;  applescript_codes[',']=43
+    ascii_codes['.']=46;  applescript_codes['.']=47
+    ascii_codes['/']=47;  applescript_codes['/']=44
+    ascii_codes['`']=96;  applescript_codes['`']=50
+    
+    # Function keys (no ASCII equivalent)
+    ascii_codes[f1]=65535;   applescript_codes[f1]=122
+    ascii_codes[f2]=65535;   applescript_codes[f2]=120
+    ascii_codes[f3]=65535;   applescript_codes[f3]=99
+    ascii_codes[f4]=65535;   applescript_codes[f4]=118
+    ascii_codes[f5]=65535;   applescript_codes[f5]=96
+    ascii_codes[f6]=65535;   applescript_codes[f6]=97
+    ascii_codes[f7]=65535;   applescript_codes[f7]=98
+    ascii_codes[f8]=65535;   applescript_codes[f8]=100
+    ascii_codes[f9]=65535;   applescript_codes[f9]=101
+    ascii_codes[f10]=65535;  applescript_codes[f10]=109
+    ascii_codes[f11]=65535;  applescript_codes[f11]=103
+    ascii_codes[f12]=65535;  applescript_codes[f12]=111
+    
+    # Special keys
+    ascii_codes[space]=32;    applescript_codes[space]=49
+    ascii_codes[tab]=9;       applescript_codes[tab]=48
+    ascii_codes[return]=13;   applescript_codes[return]=36
+    ascii_codes[enter]=13;    applescript_codes[enter]=36
+    ascii_codes[delete]=127;  applescript_codes[delete]=51
+    ascii_codes[escape]=27;   applescript_codes[escape]=53
+    ascii_codes[esc]=27;      applescript_codes[esc]=53
+    
+    # Arrow keys (no ASCII equivalent)
+    ascii_codes[up]=65535;    applescript_codes[up]=126
+    ascii_codes[down]=65535;  applescript_codes[down]=125
+    ascii_codes[left]=65535;  applescript_codes[left]=123
+    ascii_codes[right]=65535; applescript_codes[right]=124
+    ascii_codes[up-arrow]=65535;    applescript_codes[up-arrow]=126
+    ascii_codes[down-arrow]=65535;  applescript_codes[down-arrow]=125
+    ascii_codes[left-arrow]=65535;  applescript_codes[left-arrow]=123
+    ascii_codes[right-arrow]=65535; applescript_codes[right-arrow]=124
+    
+    # Modifier keys (no ASCII equivalent)
+    ascii_codes[shift]=65535;     applescript_codes[shift]=56
+    ascii_codes[control]=65535;   applescript_codes[control]=59
+    ascii_codes[option]=65535;    applescript_codes[option]=58
+    ascii_codes[command]=65535;   applescript_codes[command]=55
+    ascii_codes[fn]=65535;        applescript_codes[fn]=63
+    ascii_codes[caps-lock]=65535; applescript_codes[caps-lock]=57
+    ascii_codes[capslock]=65535;  applescript_codes[capslock]=57
+    
+    # Check if key exists in our mapping
+    if [[ -n ${ascii_codes[$key_lower]} ]]; then
+        echo "${ascii_codes[$key_lower]} ${applescript_codes[$key_lower]}"
+    else
+        echo "Error: Unknown key description '$key'" >&2
+        return 1
+    fi
+}
+
+# Helper function to get just the ASCII code for a key description
+get_ascii_code_for_key() {
+    local result=$(get_key_codes "$1")
+    if [[ $? -eq 0 ]]; then
+        echo "${result%% *}"
+    else
+        return 1
+    fi
+}
+
+# Helper function to get just the AppleScript key code
+get_applescript_code() {
+    local result=$(get_key_codes "$1")
+    if [[ $? -eq 0 ]]; then
+        echo "${result##* }"
+    else
+        return 1
+    fi
+}
+
+# Example usage:
+# get_key_codes "a"        # Returns: "97 0"
+# get_key_codes "F5"       # Returns: "65535 96"
+# get_key_codes "space"    # Returns: "32 49"
+# get_key_codes "up-arrow" # Returns: "65535 126"
+
+
 get_ascii_code_for_char() {
   # Function to get ASCII code of character in only argument.
   # Returns 65535 if character has no ASCII code (i.e., > 127)
