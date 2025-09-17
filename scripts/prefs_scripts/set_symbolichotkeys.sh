977e80539c5f882cae43e417ce53f7ab8a877f20
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-16 20:47:08 -0700

    Update set_symbolichotkeys.sh

--- scripts/prefs_scripts/set_symbolichotkeys.sh
+++ scripts/prefs_scripts/set_symbolichotkeys.sh
@@ -12,9 +12,12 @@ safe_source "${PREFS_FUNCTIONS_DIR}/symbolichotkeys_helpers.sh"
 
 ############################## BEGIN SCRIPT PROPER ##############################
 
-plistbud="/usr/libexec/PlistBuddy"
-domain="com.apple.symbolichotkeys"
-symdict="AppleSymbolicHotKeys"
+# Assumes the following environment variables are defined in, and sourced from, assign_environment_variables.sh
+# SHIFT_CHAR=$'\u21e7'     # ⇧
+# CONTROL_CHAR=$'\u2303'   # ⌃  
+# OPTION_CHAR=$'\u2325'    # ⌥
+# COMMAND_CHAR=$'\u2318'   # ⌘
+# META_MODIFIER_CHARS="${CONTROL_CHAR}${OPTION_CHAR}${COMMAND_CHAR}"
 
 
 
@@ -22,6 +25,46 @@ function set_symbolichotkeys() {
 
 report_start_phase_standard
 report_action_taken "Set hot-key correspondences to Apple commands"
+
+############### Hotkeys to disable
+
+# Minimize a window
+# disable_command_by_its_id ???
+
+# Move left a space
+disable_command_by_its_id 79
+
+# Move right a space
+disable_command_by_its_id 81
+
+# Move up a space
+disable_command_by_its_id 85
+
+# Move down a space
+disable_command_by_its_id 83
+
+############### Set new hotkeys
+
+# Set hotkeys to move to a new space by number of the space 1–16
+modifiers_for_mission_control=($META_MODIFIER_CHARS)
+assign_hotkey_to_command_id 118 "1" $modifiers_for_mission_control
+assign_hotkey_to_command_id 119 "2" $modifiers_for_mission_control
+assign_hotkey_to_command_id 120 "3" $modifiers_for_mission_control
+assign_hotkey_to_command_id 121 "4" $modifiers_for_mission_control
+assign_hotkey_to_command_id 122 "5" $modifiers_for_mission_control
+assign_hotkey_to_command_id 123 "6" $modifiers_for_mission_control
+assign_hotkey_to_command_id 124 "7" $modifiers_for_mission_control
+assign_hotkey_to_command_id 125 "8" $modifiers_for_mission_control
+assign_hotkey_to_command_id 126 "9" $modifiers_for_mission_control
+assign_hotkey_to_command_id 127 "0" $modifiers_for_mission_control
+assign_hotkey_to_command_id 128 "F1" $modifiers_for_mission_control
+assign_hotkey_to_command_id 129 "F2" $modifiers_for_mission_control
+assign_hotkey_to_command_id 130 "F3" $modifiers_for_mission_control
+assign_hotkey_to_command_id 131 "F4" $modifiers_for_mission_control
+assign_hotkey_to_command_id 132 "F5" $modifiers_for_mission_control
+assign_hotkey_to_command_id 133 "F6" $modifiers_for_mission_control
+
+
 
 report_fail "You need to finish writing this function!"
 
