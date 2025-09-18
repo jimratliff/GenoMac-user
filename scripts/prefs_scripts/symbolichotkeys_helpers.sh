3cd746bf10d9c863f6200382335d2895a0457ab2
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-18 15:09:59 -0700

    Add debug prints

--- scripts/prefs_scripts/symbolichotkeys_helpers.sh
+++ scripts/prefs_scripts/symbolichotkeys_helpers.sh
@@ -63,6 +63,9 @@ function assign_hotkey_to_command_id() {
   local key_name="$2"
   local modifier_chars="$3"
 
+  ############### DEBUG
+  report "Inside assign_hotkey… $command_ID $key_name $modifier_chars"
+
   local command_description=$(get_command_description "$command_ID")
   local ascii_and_virtual_key_codes=$(get_hotkey_ascii_and_AppleScript_key_codes "$key_name")
   local modifier_combo_mask=$(modifier_combination_to_mask "$modifier_chars" )
@@ -71,6 +74,9 @@ function assign_hotkey_to_command_id() {
   local ascii_code="${ascii_and_virtual_key_codes%% *}"
   local virtual_key_code="${ascii_and_virtual_key_codes##* }"
   xml_value=$(xml_value_for_hot_key_by_ascii_code_key_code_and_modifier_mask "$ascii_code" "$virtual_key_code" "$modifier_combo_mask")
+
+  ############### DEBUG
+  echo "$xml_value"
 
   report_adjust_setting "Set hotkey ${modifier_chars}${key_name} for command #${command_ID}: $command_description"
   modify_symbolichotkeys_entry_for_command_by_id "$command_ID" "$xml_value" ; success_or_not
