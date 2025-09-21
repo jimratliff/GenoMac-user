5fb52f37a47a5d95098d680a82b186b31d99401b
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-20 18:20:27 -0700

    Update bootstrap_dock.sh

--- scripts/prefs_scripts/bootstrap_dock.sh
+++ scripts/prefs_scripts/bootstrap_dock.sh
@@ -14,16 +14,17 @@ source "${GENOMAC_HELPER_DIR}/helpers.sh"
 # Define Dock app items
 # Each app in APPS_FOR_DOCK is referenced by its path relative to /Applications
 COMMON_PATH_FOR_APPS="/Applications/"
-APPS_FOR_DOCK=\
-  "System%20Settings.app/"
-  "1Password" \
-  "Antnotes" \
-  "TextExpander" \
-  "Raindrop.io"
-  "Obsidian"
-  "Utilities/Activity%20Monitor"
-  "Utilities/Terminal.app/"
-  "iTerm.app/"
+APPS_FOR_DOCK=(
+  "System Settings.app"
+  "1Password.app"
+  "Antnotes.app"
+  "TextExpander.app"
+  "Raindrop.io.app"
+  "Obsidian.app"
+  "Utilities/ActivityMonitor.app"
+  "Utilities/Terminal.app"
+  "iTerm.app"
+)
 
 function dock_app_entry() {
   # Function takes single argument of the full path of the app to add to the Dock.
@@ -45,16 +46,23 @@ local dock_persistent_apps_key="persistent-apps"
 
 report_action_taken "Remove all persistent apps from Dock in preparation for repopulation" 
 defaults delete $domain $dock_persistent_apps_key ; success_or_not
+
+# Policy: Leave the non-persistent part of the Dock alone.
 # report_adjust_setting "Delete file and folder entries from existing Dock"
 # defaults delete $domain $dock_ephemeral_files_folders_key ; success_or_not
 
-for app in $APPS_FOR_DOCK; do
-  app_path=${COMMON_PATH_FOR_APPS}"$app"
-  dock_item="$(dock_app_entry $app_path"
-  defaults write $domain $dock_persistent_apps_key -array-add $dock_item
+# Initialize the array
+defaults write "$domain" "$dock_persistent_apps_key" -array
+
+for app in "${APPS_FOR_DOCK[@]}"; do
+  report_adjust_setting "App $app added to Dock"
+  app_path_encoded="${COMMON_PATH_FOR_APPS}${app// /%20}"
+  dock_item="$(dock_app_entry $app_path_encoded)"
+  defaults write $domain $dock_persistent_apps_key -array-add $dock_item  ; success_or_not
+done
 
 report_action_taken "Killing Dock (metaphorically)."
-killall Dock ; success_or_not
+killall Dock 2>/dev/null || true ; success_or_not
 
 report_end_phase_standard
 }
