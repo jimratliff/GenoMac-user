05660dfbd952b92a4e9d137ce20b96bab7dd0ead
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-09-28 16:26:27 -0700

    Update set_iterm_settings.sh

--- scripts/prefs_scripts/set_iterm_settings.sh
+++ scripts/prefs_scripts/set_iterm_settings.sh
@@ -29,6 +29,8 @@ report_adjust_setting "Set Theme to Dark"
 # The default value, Regular, corresponds to 1
 defaults write ${domain} TabStyleWithAutomaticOption -int 1 ; success_or_not
 
+# The following settings apply to the Default profile
+
 report_adjust_setting "Change default font to Fira Code Nerd Font"
 
 # It is insufficient to test for the existence of the plist file. The plist file can exist but yet
@@ -43,6 +45,10 @@ if ! "${PLISTBUDDY_PATH}" -c 'Print :"New Bookmarks":0:"Normal Font"' "${plist_p
 fi
 
 "${PLISTBUDDY_PATH}" -c 'Set :"New Bookmarks":0:"Normal Font" "FiraCodeNFM-Reg 12"' "${plist_path}" ; success_or_not
+
+# Set number of scrollback lines to unlimited
+report_adjust_setting "Set number of scrollback lines to unlimited"
+"${PLISTBUDDY_PATH}" -c 'Set :"New Bookmarks":0:"Unlimited Scrollback" 1' "${plist_path}" ; success_or_not
 
 report_end_phase_standard
 
