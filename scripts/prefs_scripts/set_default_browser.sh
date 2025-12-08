588febebd2214386b3120d99ee754a5134e2255f
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-12-08 13:31:36 -0800

    Add bundle ID for Brave Browser

--- scripts/prefs_scripts/set_default_browser.sh
+++ scripts/prefs_scripts/set_default_browser.sh
@@ -13,6 +13,12 @@ source "${GENOMAC_HELPER_DIR}/helpers.sh"
 
 function set_default_browser() {
 # Sets the user’s chosen browser as the default browser. (See "$chosen_browser_id".)
+# Specify the browser’s Bundle Identifier (CFBundleIdentifier) as the argument.
+#
+# To determine the bundle ID of an app:
+#   mdls -name kMDItemCFBundleIdentifier -r /Applications/AppName.app
+#     or
+#   osascript -e 'id of app "App Name"'
 
 report_start_phase_standard
 
@@ -22,8 +28,13 @@ local path_to_installed_default_browser_utility="/opt/macadmins/bin/default-brow
 local browser_id_chrome="com.google.chrome"
 local browser_id_safari="com.apple.safari"
 local browser_id_firefox="org.mozilla.firefox"
+local browser_id_brave="com.brave.Browser"
 
+# Un-comment the line corresponding to the desired default browser
 local chosen_browser_id="$browser_id_firefox"
+# local chosen_browser_id="$browser_id_safari"
+# local chosen_browser_id="$browser_id_brave"
+# local chosen_browser_id="$browser_id_chrome"
 
 report_action_taken "Set default browser to $chosen_browser_id"
 "$path_to_installed_default_browser_utility" --identifier "$chosen_browser_id" ; success_or_not
