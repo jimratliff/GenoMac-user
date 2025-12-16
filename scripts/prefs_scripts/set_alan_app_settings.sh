902ca2c9881e4ea1261c814d3ca4163dbeebca11
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-12-15 16:23:38 -0800

    Update set_alan_app_settings.sh

--- scripts/prefs_scripts/set_alan_app_settings.sh
+++ scripts/prefs_scripts/set_alan_app_settings.sh
@@ -118,7 +118,8 @@ report_adjust_setting "Set: color of border highlight when in Light Mode"
 defaults write "${domain}" lightMode -data "$(printf '%s' "$color_lightMode_base64" | base64 -D | xxd -p | tr -d '\n')" ; success_or_not
 
 report_adjust_setting "Set: color of border highlight when in Dark Mode"
-defaults write "${domain}" darkMode -data "$(printf '%s' "$color_darkMode_base64" | base64 -D)" ; success_or_not
+# defaults write "${domain}" darkMode -data "$(printf '%s' "$color_darkMode_base64" | base64 -D)" ; success_or_not
+defaults write "${domain}" lightMode -data "$(printf '%s' "$color_darkMode_base64" | base64 -D | xxd -p | tr -d '\n')" ; success_or_not
 
 report_end_phase_standard
 
