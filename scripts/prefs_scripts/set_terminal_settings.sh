5b656abe09b8e3e7c5b82a23d3ec92cce65fa61a
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-12-19 16:09:20 -0800

    Fix placements of success_or_not

--- scripts/prefs_scripts/set_terminal_settings.sh
+++ scripts/prefs_scripts/set_terminal_settings.sh
@@ -18,10 +18,10 @@ report_start_phase_standard
 local domain="com.apple.Terminal"
 
 report_action_taken "Give the Terminal a teeny bit of style, even though we will soon abandon it"
-report_adjust_setting "Terminal: default for new windows: “Man Page”";success_or_not
-defaults write "${domain}" "Default Window Settings" -string "Man Page"
-report_adjust_setting "Terminal: default for starting windows: “Man Page”";success_or_not
-defaults write "${domain}" "Startup Window Settings" -string "Man Page"
+report_adjust_setting "Terminal: default for new windows: “Man Page”"
+defaults write "${domain}" "Default Window Settings" -string "Man Page" ; success_or_not
+report_adjust_setting "Terminal: default for starting windows: “Man Page”"
+defaults write "${domain}" "Startup Window Settings" -string "Man Page" ; success_or_not
 
 report_end_phase_standard
 
