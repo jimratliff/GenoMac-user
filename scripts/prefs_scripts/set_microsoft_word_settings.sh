6e4a7a83def7a778d4ca86131a8f6088a8179d33
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-12-21 22:09:19 -0800

    Update set_microsoft_word_settings.sh

--- scripts/prefs_scripts/set_microsoft_word_settings.sh
+++ scripts/prefs_scripts/set_microsoft_word_settings.sh
@@ -17,10 +17,26 @@ report_start_phase_standard
 
 local domain="com.microsoft.Word"
 
-report_action_taken "Set VERY limited Microsoft Word preferences"
+report_action_taken "Set VERY limited Microsoft Word preferences with defaults write command(s)"
 
 report_adjust_setting "Ribbon: Show group titles"
 defaults write "${domain}" OUIRibbonShowGroupTitles -bool true ; success_or_not
+
+report_action_taken "Use a VBA script to implement additional Microsoft Word preferences"
+
+local Word_document_filename="Container_for_VBA_macro_to_set_Word_preferences.docm"
+
+# Construct path to macro-containing Word file
+local source_path="${GENOMAC_USER_RESOURCE_DIRECTORY}/microsoft_word/${Word_document_filename}"
+
+# Confirm existence of macro-containing Word file
+if [[ ! -f "$source_path" ]]; then
+  report_fail "Missing macro-containing Word document at ${source_path}"
+  exit 1
+fi
+
+# Open the document in Word (this triggers Document_Open → SetMyPreferences)
+open -a "Microsoft Word" "$source_path"
 
 report_end_phase_standard
 
