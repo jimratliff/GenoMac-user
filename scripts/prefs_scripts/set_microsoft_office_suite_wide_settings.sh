7511e908e71c68f6951fca60b865d8861172b4fc
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-12-26 10:20:34 -0800

    Indent body of function set_microsoft_office_suite_wide_settings()

--- scripts/prefs_scripts/set_microsoft_office_suite_wide_settings.sh
+++ scripts/prefs_scripts/set_microsoft_office_suite_wide_settings.sh
@@ -13,33 +13,33 @@ source "${GENOMAC_HELPER_DIR}/helpers.sh"
 
 function set_microsoft_office_suite_wide_settings() {
 
-report_start_phase_standard
-
-local domain="com.microsoft.office"
-
-report_action_taken "Set preferences that affect all Microsoft 364 Office applications"
-
-report_adjust_setting "Automatically sign in (suppressing “do you want to buy?” welcome dialog)"
-defaults write "${domain}" OfficeAutoSignIn -bool true ; success_or_not
-
-# For later implementation, after I develop a way to:
-# - in an initial bootstrap-only step, ask user for email address
-# - write that email address to ~/.config/microsoft-office/login_email.txt
-# - in this script, look for that that login_email.txt file and read the address
-# - if successfully found, use that email address in the below. Otherwise skip…
-# local microsoft_office_login_email_address
-# report_adjust_setting "Set Microsoft 365 sign-in name to be added on first launch"
-# defaults write "${domain}" OfficeActivationEmailAddress -string ${microsoft_office_login_email_address} ; success_or_not
-
-report_adjust_setting "Don’t show template gallery automatically on app launch"
-defaults write "${domain}" ShowDocStageOnLaunch -bool false ; success_or_not
-
-report_adjust_setting "Set default file location to local, not cloud-based"
-defaults write "${domain}" ShowDocStageOnLaunch -bool false ; success_or_not
-
-report_adjust_setting "Show file extensions"
-defaults write "${domain}" ShowFileExtensions -bool true ; success_or_not
-
-report_end_phase_standard
+	report_start_phase_standard
+	
+	local domain="com.microsoft.office"
+	
+	report_action_taken "Set preferences that affect all Microsoft 364 Office applications"
+	
+	report_adjust_setting "Automatically sign in (suppressing “do you want to buy?” welcome dialog)"
+	defaults write "${domain}" OfficeAutoSignIn -bool true ; success_or_not
+	
+	# For later implementation, after I develop a way to:
+	# - in an initial bootstrap-only step, ask user for email address
+	# - write that email address to ~/.config/microsoft-office/login_email.txt
+	# - in this script, look for that that login_email.txt file and read the address
+	# - if successfully found, use that email address in the below. Otherwise skip…
+	# local microsoft_office_login_email_address
+	# report_adjust_setting "Set Microsoft 365 sign-in name to be added on first launch"
+	# defaults write "${domain}" OfficeActivationEmailAddress -string ${microsoft_office_login_email_address} ; success_or_not
+	
+	report_adjust_setting "Don’t show template gallery automatically on app launch"
+	defaults write "${domain}" ShowDocStageOnLaunch -bool false ; success_or_not
+	
+	report_adjust_setting "Set default file location to local, not cloud-based"
+	defaults write "${domain}" ShowDocStageOnLaunch -bool false ; success_or_not
+	
+	report_adjust_setting "Show file extensions"
+	defaults write "${domain}" ShowFileExtensions -bool true ; success_or_not
+	
+	report_end_phase_standard
 
 }
