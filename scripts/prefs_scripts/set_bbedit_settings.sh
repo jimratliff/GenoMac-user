99254fd8b2bd0c289e34b64b7aab7969d5b08aa0
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-12-26 20:41:33 -0800

    Fix indentation of body of function set_bbedit_settings()

--- scripts/prefs_scripts/set_bbedit_settings.sh
+++ scripts/prefs_scripts/set_bbedit_settings.sh
@@ -15,26 +15,26 @@ source "${GENOMAC_HELPER_DIR}/helpers.sh"
 
 function set_bbedit_settings() {
 
-report_start_phase_standard
-report_action_taken "Implement BBEdit settings"
-
-local domain="com.barebones.bbedit"
-
-report_adjust_setting "Set: Do NOT prefer shared window for New and Open"
-defaults write ${domain} NewAndOpenPrefersSharedWindow -bool false ; success_or_not
-
-report_adjust_setting "Set: DO soft-wrap text"
-defaults write ${domain} EditorSoftWrap -bool true ; success_or_not
-
-report_adjust_setting "Set: Soft-wrap text to WINDOW WIDTH"
-defaults write ${domain} SoftWrapStyle -int 2 ; success_or_not
-
-report_adjust_setting "Set: Display full path of files in “Open Recent” Items"
-defaults write ${domain} Display_FullPathInRecentMenu -bool true ; success_or_not
-
-report_adjust_setting "Set: When BBEdit becomes active, do nothing (don’t open a new doc)"
-defaults write ${domain} StartupAndResumeAction -int 1 ; success_or_not
-
-report_end_phase_standard
+  report_start_phase_standard
+  report_action_taken "Implement BBEdit settings"
+  
+  local domain="com.barebones.bbedit"
+  
+  report_adjust_setting "Set: Do NOT prefer shared window for New and Open"
+  defaults write ${domain} NewAndOpenPrefersSharedWindow -bool false ; success_or_not
+  
+  report_adjust_setting "Set: DO soft-wrap text"
+  defaults write ${domain} EditorSoftWrap -bool true ; success_or_not
+  
+  report_adjust_setting "Set: Soft-wrap text to WINDOW WIDTH"
+  defaults write ${domain} SoftWrapStyle -int 2 ; success_or_not
+  
+  report_adjust_setting "Set: Display full path of files in “Open Recent” Items"
+  defaults write ${domain} Display_FullPathInRecentMenu -bool true ; success_or_not
+  
+  report_adjust_setting "Set: When BBEdit becomes active, do nothing (don’t open a new doc)"
+  defaults write ${domain} StartupAndResumeAction -int 1 ; success_or_not
+  
+  report_end_phase_standard
 
 }
