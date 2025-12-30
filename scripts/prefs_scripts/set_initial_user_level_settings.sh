a758a76a7c692ed29897bdc4a6a50ccab238400e
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-12-29 19:46:34 -0800

    Remove "(VERY limited)" caveat from commeny

--- scripts/prefs_scripts/set_initial_user_level_settings.sh
+++ scripts/prefs_scripts/set_initial_user_level_settings.sh
@@ -189,7 +189,7 @@ set_keyboard_maestro_settings
 # Microsoft Office
 # Suite-wide-settings
 set_microsoft_office_suite_wide_settings
-# Microsoft Word (VERY limited)
+# Microsoft Word
 set_microsoft_word_settings
 
 # OmniOutliner
