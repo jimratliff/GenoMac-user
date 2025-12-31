a5383f0289c4c38f30476700c76c08cc5efab4dc
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-12-30 21:11:24 -0800

    Create reset_state.sh

---
+++ scripts/reset_state.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env zsh
+
+# Script to be executed only one time per user.
+
+# Fail early on unset variables or command failure
+set -euo pipefail
+
+# Resolve this script's directory (even if sourced)
+this_script_path="${0:A}"
+this_script_dir="${this_script_path:h}"
+
+# Assign environment variables (including GENOMAC_HELPER_DIR).
+# Assumes that assign_environment_variables.sh is in same directory as the
+# current script.
+source "${this_script_dir}/assign_environment_variables.sh"
+
+# Source helpers
+source "${GENOMAC_HELPER_DIR}/helpers.sh"
+
+# Specify the directory in which the file(s) containing the preferences-related 
+# functions called by this script lives.
+# E.g., the function `overrides_for_sysadmin_users` is supplied by a file 
+# `overrides_for_sysadmin_users.sh`. If `overrides_for_sysadmin_users.sh` 
+# resides at the same level as this script:
+# PREFS_FUNCTIONS_DIR="${this_script_dir}"
+PREFS_FUNCTIONS_DIR="${this_script_dir}/prefs_scripts"
+
+# Print assigned paths for diagnostic purposes
+# printf "\n📂 Path diagnostics:\n"
+# printf "this_script_dir:              %s\n" "$this_script_dir"
+# printf "GENOMAC_HELPER_DIR: %s\n" "$GENOMAC_HELPER_DIR"
+# printf "PREFS_FUNCTIONS_DIR:  %s\n\n" "$PREFS_FUNCTIONS_DIR"
+
+############################## BEGIN SCRIPT PROPER #############################
+
+report_start_phase_standard
+
+reset_state ; success_or_not
+
+report_end_phase 'Completed: Local state has been reset.' 
+
+dump_accumulated_warnings_failures
