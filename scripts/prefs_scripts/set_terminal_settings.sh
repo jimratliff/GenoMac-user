3826824dd5069a0ede9f36427e8bce00009e26e7
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-05 13:00:01 -0800

    Update set_terminal_settings.sh

--- scripts/prefs_scripts/set_terminal_settings.sh
+++ scripts/prefs_scripts/set_terminal_settings.sh
@@ -1,28 +1,17 @@
-# This file assumes GENOMAC_HELPER_DIR is already set in the current shell
-# to the absolute path of the directory containing helpers.sh.
-# That variable must be defined before this file is sourced.
-
-if [[ -z "${GENOMAC_HELPER_DIR:-}" ]]; then
-  echo "❌ GENOMAC_HELPER_DIR is not set. Please source `initial_prefs.sh` first."
-  return 1
-fi
-
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
-
-############################## BEGIN SCRIPT PROPER ##############################
+⇧⌥⌘F2
 
 function set_terminal_settings() {
 
-report_start_phase_standard
-
-local domain="com.apple.Terminal"
-
-report_action_taken "Give the Terminal a teeny bit of style, even though we will soon abandon it"
-report_adjust_setting "Terminal: default for new windows: “Man Page”"
-defaults write "${domain}" "Default Window Settings" -string "Man Page" ; success_or_not
-report_adjust_setting "Terminal: default for starting windows: “Man Page”"
-defaults write "${domain}" "Startup Window Settings" -string "Man Page" ; success_or_not
-
-report_end_phase_standard
+  report_start_phase_standard
+  
+  local domain="com.apple.Terminal"
+  
+  report_action_taken "Give the Terminal a teeny bit of style, even though we will soon abandon it"
+  report_adjust_setting "Terminal: default for new windows: “Man Page”"
+  defaults write "${domain}" "Default Window Settings" -string "Man Page" ; success_or_not
+  report_adjust_setting "Terminal: default for starting windows: “Man Page”"
+  defaults write "${domain}" "Startup Window Settings" -string "Man Page" ; success_or_not
+  
+  report_end_phase_standard
 
 }
