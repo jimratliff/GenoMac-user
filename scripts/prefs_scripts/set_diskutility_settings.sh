b63f67372148646fdb528caf2372d2b9a7bdc9b3
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-05 09:49:40 -0800

    Update set_diskutility_settings.sh

--- scripts/prefs_scripts/set_diskutility_settings.sh
+++ scripts/prefs_scripts/set_diskutility_settings.sh
@@ -1,34 +1,23 @@
-# This file assumes GENOMAC_HELPER_DIR is already set in the current shell
-# to the absolute path of the directory containing helpers.sh.
-# That variable must be defined before this file is sourced.
-
-if [[ -z "${GENOMAC_HELPER_DIR:-}" ]]; then
-  echo "❌ GENOMAC_HELPER_DIR is not set. Please source `initial_prefs.sh` first."
-  return 1
-fi
-
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
-
-############################## BEGIN SCRIPT PROPER ##############################
+#!/bin/zsh
 
 function set_diskutility_settings() {
 
-report_start_phase_standard
-report_action_taken "Implement DiskUtility settings"
-
-local domain="com.apple.DiskUtility"
-local plist_path=$(legacy_plist_path_from_domain "$domain")
-
-ensure_plist_path_exists "${plist_path}"
-
-# Show all devices in sidebar
-report_adjust_setting "DiskUtility: Show all devices in sidebar"
-defaults write ${domain} SidebarShowAllDevices -bool true ; success_or_not
-
-# Show hidden partitions (untested)
-report_adjust_setting "DiskUtility: Show hidden partitions"
-defaults write ${domain} DUShowEveryPartition -boolean true ; success_or_not
-
-report_end_phase_standard
+  report_start_phase_standard
+  report_action_taken "Implement DiskUtility settings"
+  
+  local domain="com.apple.DiskUtility"
+  local plist_path=$(legacy_plist_path_from_domain "$domain")
+  
+  ensure_plist_path_exists "${plist_path}"
+  
+  # Show all devices in sidebar
+  report_adjust_setting "DiskUtility: Show all devices in sidebar"
+  defaults write ${domain} SidebarShowAllDevices -bool true ; success_or_not
+  
+  # Show hidden partitions (untested)
+  report_adjust_setting "DiskUtility: Show hidden partitions"
+  defaults write ${domain} DUShowEveryPartition -boolean true ; success_or_not
+  
+  report_end_phase_standard
 
 }
