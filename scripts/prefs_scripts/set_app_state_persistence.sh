cb286929f35aeffaeb94b40fba74f0559310ae35
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-04 20:32:41 -0800

    Update set_app_state_persistence.sh

--- scripts/prefs_scripts/set_app_state_persistence.sh
+++ scripts/prefs_scripts/set_app_state_persistence.sh
@@ -1,32 +1,24 @@
-# This file assumes GENOMAC_HELPER_DIR is already set in the current shell
-# to the absolute path of the directory containing helpers.sh.
-# That variable must be defined before this file is sourced.
-
-if [[ -z "${GENOMAC_HELPER_DIR:-}" ]]; then
-  echo "❌ GENOMAC_HELPER_DIR is not set. Please source `initial_prefs.sh` first."
-  return 1
-fi
-
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
-
-############################## BEGIN SCRIPT PROPER ##############################
+#!/bin/zsh
 
 function set_app_state_persistence() {
 
-report_start_phase_standard
-report_action_taken "Implement app-state persistence"
+  # Specifies policies for app-state persistence, i.e., whether a window or app reopens after restart
+  # I _do_ want windows/apps to reopen. I believe this is the default and that the below reinforces defaults.
 
-report_adjust_setting "1 of 3: loginwindow: TALLogoutSavesState: true"
-defaults write com.apple.loginwindow TALLogoutSavesState -bool true;success_or_not
-report_adjust_setting "2 of 3: loginwindow: LoginwindowLaunchesRelaunchApps: true"
-defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool true;success_or_not
-report_adjust_setting "3 of 3: NSGlobalDomain: NSQuitAlwaysKeepsWindows: true"
-defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool true;success_or_not
-
-# Closing a document window confirms any dirty changes
-report_adjust_setting "Implement document-state persistence"
-defaults write NSGlobalDomain NSCloseAlwaysConfirmsChanges -bool true;success_or_not
-
-report_end_phase_standard
+  report_start_phase_standard
+  report_action_taken "Implement app-state persistence"
+  
+  report_adjust_setting "1 of 3: loginwindow: TALLogoutSavesState: true"
+  defaults write com.apple.loginwindow TALLogoutSavesState -bool true ; success_or_not
+  report_adjust_setting "2 of 3: loginwindow: LoginwindowLaunchesRelaunchApps: true"
+  defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool true ; success_or_not
+  report_adjust_setting "3 of 3: NSGlobalDomain: NSQuitAlwaysKeepsWindows: true"
+  defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool true ; success_or_not
+  
+  # Closing a document window confirms any dirty changes
+  report_adjust_setting "Implement document-state persistence"
+  defaults write NSGlobalDomain NSCloseAlwaysConfirmsChanges -bool true ; success_or_not
+  
+  report_end_phase_standard
 
 }
