458d85b18cb42229e62fb7bed5a12413a582111e
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-05 21:31:19 -0800

    Update run_hypervisor.sh

--- scripts/run_hypervisor.sh
+++ scripts/run_hypervisor.sh
@@ -3,24 +3,39 @@
 # Fail early on unset variables or command failure
 set -euo pipefail
 
-# Template for entry-point scripts
-
 source "${HOME}/.genomac-user/scripts/0_initialize_me.sh"
 
 ############################## BEGIN SCRIPT PROPER ##############################
 
 # Source required files
-# safe_source "${GMU_PREFS_SCRIPTS}/install_btt_license.sh"
+safe_source "${GMU_SCRIPTS_DIR}/stow_dotfiles.sh"
+safe_source "${GMU_PREFS_SCRIPTS}/ask_initial_questions.sh"
+
+local NAME_OF_PRESENT_MAKE_COMMAND="make run-hypervisor"
+
+local welcome_message="Welcome"
+if test_genomac_user_state "$GNU_SESH_SESSION_HAS_STARTED"; then
+  welcome_message="Welcome back"
+else
+  set_genomac_user_state "$GNU_SESH_SESSION_HAS_STARTED"
+fi
+
+report "${welcome_message} to the GenoMac-user Hypervisor!"
+report "To get back into the groove at any time, just reexecute ${NAME_OF_PRESENT_MAKE_COMMAND}\nand we’ll pick up where we left off."
 
 if ! test_genomac_user_state "$GMU_PERM_INTRO_QUESTIONS_ASKED_AND_ANSWERED"; then
   ask_initial_questions
   set_genomac_user_state "$GMU_PERM_INTRO_QUESTIONS_ASKED_AND_ANSWERED"
+else
+  report_action_taken "Skipping introductory questions, because you’ve already answered them."
 fi
 
 if ! test_genomac_user_state "$GNU_SESH_DOTFILES_HAVE_BEEN_STOWED"; then 
   ensure_zshenv_loaded
   stow_packages_dotfiles
   set_genomac_user_state "$GNU_SESH_DOTFILES_HAVE_BEEN_STOWED"
+else
+  report_action_taken "Skipping stowing dotfiles, because you’ve already stowed them."
 fi
 
 function some_function() {
