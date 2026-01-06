95e3aeed112717d87378d72c7a0ffea1afa17a8d
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-05 20:18:51 -0800

    Update run_hypervisor.sh

--- scripts/run_hypervisor.sh
+++ scripts/run_hypervisor.sh
@@ -1,71 +1,39 @@
-#!/usr/bin/env zsh
-
-# Implements an initial set of selected `defaults` and related commands for each 
-# user. Specifically, the settings covered by this script pertain only to the
-# the operating system and to applications that come pre-installed on a new
-# out-of-the-box Mac.
+#!/usr/bin/env zs
 
 # Fail early on unset variables or command failure
 set -euo pipefail
 
-# Resolve this script's directory (even if sourced)
-this_script_path="${0:A}"
-this_script_dir="${this_script_path:h}"
+# Template for entry-point scripts
 
-# Assign environment variables (including GENOMAC_HELPER_DIR).
-# Assumes that assign_environment_variables.sh is in same directory as the
-# current script.
-source "${this_script_dir}/assign_environment_variables.sh"
+source "${HOME}/.genomac-user/scripts/0_initialize_me.sh"
 
-# Assign environment variables that correspond to states
-# Assumes that assign_enum_env_vars_for_states.sh is in same directory as the
-# current script.
-source "${this_script_dir}/assign_enum_env_vars_for_states.sh"
+############################## BEGIN SCRIPT PROPER ##############################
 
-# Source helpers
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
+# Source required files
+# safe_source "${GMU_PREFS_SCRIPTS}/install_btt_license.sh"
 
-# Specify the directory in which the file(s) containing the preferences-related 
-# functions called by this script lives.
-# E.g., the function `overrides_for_sysadmin_users` is supplied by a file 
-# `overrides_for_sysadmin_users.sh`. If `overrides_for_sysadmin_users.sh` 
-# resides at the same level as this script:
-# PREFS_FUNCTIONS_DIR="${this_script_dir}"
-PREFS_FUNCTIONS_DIR="${this_script_dir}/prefs_scripts"
+if ! test_genomac_user_state "$GMU_PERM_INTRO_QUESTIONS_ASKED_AND_ANSWERED"; then
+  ask_initial_questions
+  set_genomac_user_state "$GMU_PERM_INTRO_QUESTIONS_ASKED_AND_ANSWERED"
+fi
 
-# Print assigned paths for diagnostic purposes
-printf "\n📂 Path diagnostics:\n"
-printf "this_script_dir:              %s\n" "$this_script_dir"
-printf "GENOMAC_HELPER_DIR: %s\n" "$GENOMAC_HELPER_DIR"
-printf "PREFS_FUNCTIONS_DIR:  %s\n\n" "$PREFS_FUNCTIONS_DIR"
+if ! test_genomac_user_state "$GNU_SESH_DOTFILES_HAVE_BEEN_STOWED"; then 
+  ensure_zshenv_loaded
+  stow_packages_dotfiles
+  set_genomac_user_state "$GNU_SESH_DOTFILES_HAVE_BEEN_STOWED"
+fi
 
-safe_source "${PREFS_FUNCTIONS_DIR}/set_initial_user_level_settings.sh"
-safe_source "${PREFS_FUNCTIONS_DIR}/overrides_for_sysadmin_users.sh"
+function some_function() {
+  report_start_phase_standard
 
-############################## BEGIN SCRIPT PROPER #############################
-report_start_phase 'Begin the initial preference-setting phase'
+  report "I am doing something important"
 
-# com.apple.universalaccess and com.apple.safari domains now require sudo
-report_action_taken "Some commands will require 'sudo'"
-keep_sudo_alive
+  report_end_phase_standard
 
-# Set initial user-level settings
-set_initial_user_level_settings
+}
 
-# Override certain settings in a way appropriate for only SysAdmin accounts
-# TO DO: TODO: This needs to be made conditional on which user it is.
-# overrides_for_sysadmin_users
+function main() {
+  some_function
+}
 
-report_end_phase 'Completed: the preference-setting phase of the bootstrapping process.' 
-report "It’s possible that some settings won’t take effect until after you logout or restart."
-
-dump_accumulated_warnings_failures
-
-report "Logging out to apply those changes..."
-echo ""
-echo "ℹ️  You will be logged out automatically to take into account the changed settings."
-echo "   After logging back in, continue with the next configuration step."
-echo ""
-
-force_user_logout
-
+main
