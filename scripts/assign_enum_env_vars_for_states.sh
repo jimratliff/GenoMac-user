43b3f250fe9604ae42dc6f0e44eff99ba12525cb
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-05 21:32:26 -0800

    Update assign_enum_env_vars_for_states.sh

--- scripts/assign_enum_env_vars_for_states.sh
+++ scripts/assign_enum_env_vars_for_states.sh
@@ -4,6 +4,66 @@
 # to states.
 #
 # Assumes that export_and_report() has already been made available
+#
+# There are two kinds of steps:
+# - Bootstrap
+# 	- Typically executed once per user (on a given Mac)
+# 		- Examples
+# 			- a setting that is initialized to a default value but where it is anticipated that
+# 			  the user may override that setting and would not want their override to itself
+# 			  be overridden by re-running the original configuration script
+# 			  - e.g., a Dock lineup, toolbar configurations of particular apps
+# 			- an action that, despite being idempotent (in a sense), you wouldn’t want to repeat
+# 			  because the action is time consuming or otherwise costly
+# 			  - e.g., setting the default browser (I don’t know why this is so time consuming, 
+# 			    but it is!)
+# 			  - e.g., registering a QuickLook plugin (costly because launching and then quitting 
+# 			    an app takes time)
+# 			- a manual configuration that can’t be automated (and therefore is too expensive for
+# 			  thoughtless repetition, even if doing so would be idempotent)
+# 			  - e.g., authentication of an app or external service
+# 			  - e.g., implementing a setting that isn’t exposed to scripting (e.g., granting 
+# 			    full-disk access to an app)
+# - Maintenance
+# 	- Must be idempotent
+# 	- Repeated periodically either (a) to enforce previously set settings or (b) to reflect
+# 	  recent changes
+# 	  - Examples:
+# 	  	- stowing dotfiles
+# 	  	- `defaults write` commands, where you want to continually enforce the choices 
+# 	  	  specified by GenoMac-user, even if the user has deviated from those
+# 	  	  
+# 	  	  
+# The key of each state:
+# - begins with either
+#   - `GMS_` (GenoMac-system) or
+#   - `GMU_` (GenoMac-user)
+# - immediately followed by either:
+# 	- `PERM_` for “permanent”
+# 		- Such a `GMx_PERM_` key persists across sessions. It is reset (i.e., deleted) only
+# 		  if an extraordinary circumstance arises that, after deliberation, is found to 
+# 		  warrant the repetition of this otherwise-bootstrap step.
+# 	- `SESH_` for “session”
+# 		- is cleared/reset/deleted as the final step of each successful session (or possibly
+# 		  at the beginning of each “new” session, though it’s not clear how to easily 
+# 		  determine that—without determining that the current SESH states take you all the 
+# 		  way to the end)
+# 		- `SESH_` states provide a mechanism such that the hypervisor script can be re-entered
+# 			(for example, following an enforced logout) and be able to determine which steps
+# 			can be skipped over and where to pick up the remaining sequence.
+# 		  
+# The state mechanism is managed by the repository (i.e., GenoMac-system or GenoMac-user).
+# Therefore the state mechanism doesn’t cover the earliest stages of the GenoMac-system
+# or GenoMac-user processes that instruct/direct the user to achieve the local cloning
+# of the repository.
+# 
+# 
+# - `GMx_SESH_intro_questions_have_been_answered`
+# 	- The initial set of questions from the script to the executing user have been
+# 	  comprehensively asked and answer.
+# - `GMU_PERM_user_is_sysadmin`
+# - `GMU_SESH_dotfiles_have_been_stowed`
+# 	- The dotfiles have been stowed	
 
 set -euo pipefail
 
