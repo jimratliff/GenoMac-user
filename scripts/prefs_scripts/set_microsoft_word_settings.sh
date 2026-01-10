20bd54b815613d0689c60544134980a60cbc18e0
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-09 21:34:06 -0800

    Update set_microsoft_word_settings.sh

--- scripts/prefs_scripts/set_microsoft_word_settings.sh
+++ scripts/prefs_scripts/set_microsoft_word_settings.sh
@@ -1,6 +1,38 @@
 #!/bin/zsh
 
+safe_source "${GMU_PREFS_SCRIPTS}/set_microsoft_office_suite_wide_settings.sh"
+
 GENOMAC_USER_LOCAL_MICROSOFT_WORD_RESOURCE_DIRECTORY="$GENOMAC_USER_LOCAL_RESOURCE_DIRECTORY/microsoft_word"
+
+function conditionally_configure_microsoft_word() {
+  report_start_phase_standard
+
+  if ! test_genomac_user_state "$GMU_PERM_MICROSOFT_WORD_USER_WANTS_IT"; then
+    report_action_taken "Skipping Microsoft Word configuration, because this user doesn’t want it"
+    report_end_phase_standard
+    exit 0
+  fi
+
+  if test_genomac_user_state "$GMU_PERM_MICROSOFT_WORD_HAS_BEEN_CONFIGURED"; then
+    report_action_taken "Skipping Microsoft Word configuration, because it’s already been configured and it’s a bootstrapping step"
+    report_end_phase_standard
+    exit 0
+  fi
+
+  if ! test_genomac_user_state "$GMU_PERM_MICROSOFT_WORD_HAS_BEEN_AUTHENTICATED"; then
+    # You can’t change Microsoft Word’s settings unless the app is first authenticated
+    launch_app_and_prompt_user_to_authenticate "$BUNDLE_ID_MICROSOFT_WORD" \
+      "I will launch Microsoft Word. Please log in to your Microsoft 365 account. This is necessary for me to set its preferences"
+    set_genomac_user_state "$GMU_PERM_MICROSOFT_WORD_HAS_BEEN_AUTHENTICATED"
+  fi
+
+  set_microsoft_office_suite_wide_settings
+  set_microsoft_word_settings
+
+  set_genomac_user_state "$GMU_PERM_MICROSOFT_WORD_HAS_BEEN_CONFIGURED"
+
+  report_end_phase_standard
+}
 
 function set_microsoft_word_settings() {
 
