62f297fba1048f2a4041706485cbff6a57121df4
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-11 10:16:21 -0800

    Create test_launch_and_prompt.sh

---
+++ scripts/test_launch_and_prompt.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zs
+
+# Fail early on unset variables or command failure
+set -euo pipefail
+
+# Template for entry-point scripts
+
+source "${HOME}/.genomac-user/scripts/0_initialize_me.sh"
+
+############################## BEGIN SCRIPT PROPER ##############################
+
+# Source required files
+# safe_source "${GMU_PREFS_SCRIPTS}/install_btt_license.sh"
+
+function test_launch__app_and_prompt_user_to_act() {
+  report_start_phase_standard
+
+  launch_app_and_prompt_user_to_act "$BUNDLE_ID_TEXTEDIT" "Please do the thing"
+  launch_app_and_prompt_user_to_act --show-doc "${GENOMAC_USER_LOCAL_DOCUMENTATION_DIRECTORY}/TextExpander_how_to_configure.md" "$BUNDLE_ID_TEXTEDIT" "Please do the thing"
+  
+  report_end_phase_standard 
+
+}
+
+function main() {
+  test_state_management
+}
+
+main
