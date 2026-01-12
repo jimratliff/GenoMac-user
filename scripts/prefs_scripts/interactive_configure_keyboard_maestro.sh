953682d17e1295253d75ef312b27bf901ec0be2f
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-11 21:48:31 -0800

    Update interactive_configure_keyboard_maestro.sh

--- scripts/prefs_scripts/interactive_configure_keyboard_maestro.sh
+++ scripts/prefs_scripts/interactive_configure_keyboard_maestro.sh
@@ -1,6 +1,42 @@
 #!/bin/zsh
 
+function conditionally_configure_keyboard_maestro() {
+  report_start_phase_standard
+  _run_if_not_already_done \
+    "$GMU_PERM_KEYBOARD_MAESTRO_HAS_BEEN_CONFIGURED" \
+    interactive_configure_keyboard_maestro \
+    "Skipping bootstrapping of Keyboard Maestro, because this has been done in the past."
+  report_end_phase_standard
+}
+
+function interactive_configure_keyboard_maestro() {
+  # Bootstrap Keyboard Maestro for (a) authentication and (b) macro syncing
+  # Because macro syncing relies on the existence of a directory in the user’s Dropbox directory,
+  # this bootstrapping step must wait until Dropbox is configured for the user. (It is up
+  # to the hypervisor to perform this check before calling this function.)
+  
+  report_start_phase_standard
+  report_action_taken "I will bootstrap Keyboard Maestro for (a) authentication and (b) macro syncing"
+
+  # Experimental: It’s untested whether the following `defaults write` will, on its own, configure
+  # macro syncing with the local directory
+  enable_keyboard_maestro_macro_syncing
+
+  # Interactively prompt the user to authenticate Keyboard Maestro
+  launch_app_and_prompt_user_to_act \
+    --show-doc "${GENOMAC_USER_LOCAL_DOCUMENTATION_DIRECTORY}/Keyboard_Maestro_how_to_configure.md" \
+    "$BUNDLE_ID_KEYBOARDMAESTRO_EDITOR" \
+    "Follow the instructions in the Quick Look window to log into and configure Keyboard Maestro"
+  
+  
+
+  report_end_phase_standard
+}
+
 function set_keyboard_maestro_settings() {
+
+  # This function performs the idempotent maintenace steps for Keyboard Maestro, which is
+  # separate from the bootstrapping steps of (a) authentication and (b) setting up macro syncing
 
   report_start_phase_standard
   report_action_taken "Implement Keyboard Maestro settings"
