da32b6e3ab6b20131bb82a5c02db3721bb1f80f5
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-11 21:01:43 -0800

    Create set_plain_text_editor_settings.sh

---
+++ scripts/prefs_scripts/set_plain_text_editor_settings.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+function set_plain_text_editor_settings() {
+
+  report_start_phase_standard
+  report_action_taken "Implement Plain Text Editor settings"
+
+  local domain="com.sindresorhus.Plain-Text-Editor"
+
+  report_adjust_setting "Editor » Text » Optimal line length: Turn OFF"
+  defaults write "${domain}" optimalLineLength -bool false ; success_or_not
+
+  report_adjust_setting "Editor » Interface » Content padding: Turn OFF"
+  defaults write "${domain}" useContentPadding -bool false ; success_or_not
+
+  report_adjust_setting "Editor » Stats » Line count: Turn ON"
+  defaults write ${domain} enabledStats -array lineCount characterCount wordCount ; success_or_not
+  
+  report_end_phase_standard
+}
