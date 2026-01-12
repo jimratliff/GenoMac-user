1ae153da7938b9c05fe6c5268552a16237f2222a
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-12 14:23:18 -0800

    Create dev_prep_keyboard_maestro_for_experiments.sh

---
+++ scripts/dev_prep_keyboard_maestro_for_experiments.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zs
+
+# Fail early on unset variables or command failure
+set -euo pipefail
+
+# Template for entry-point scripts
+
+source "${HOME}/.genomac-user/scripts/0_initialize_me.sh"
+
+############################## BEGIN SCRIPT PROPER ##############################
+
+# Source required files
+# safe_source "${GMU_PREFS_SCRIPTS}/install_btt_license.sh"
+
+function some_function() {
+  report_start_phase_standard
+
+  report "I am doing something important"
+
+  report_end_phase_standard
+
+}
+
+function main() {
+  some_function
+}
+
+main
