c23eb455938b29ca3351e9fa6ae828cf4e1fd919
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-13 13:05:52 -0800

    Update run_hypervisor.sh

--- scripts/run_hypervisor.sh
+++ scripts/run_hypervisor.sh
@@ -63,7 +63,7 @@ function run_hypervisor() {
   ############### Modify Desktop for certain users
   if test_genomac_user_state "$GMU_PERM_FINDER_SHOW_DRIVES_ON_DESKTOP"; then
     _run_if_not_already_done \
-      "$GMU_SESH_SHOW_DRIVES_ON_DESKTOP_HAS_BEEN_IMPLEMENTED" \
+      "$GMU_SESH_FINDER_SHOW_DRIVES_ON_DESKTOP_HAS_BEEN_IMPLEMENTED" \
       reverse_disk_display_policy_for_some_users \
       "Skipping displaying internal/external drives on Desktop, because I’ve already done so this session"
   else
