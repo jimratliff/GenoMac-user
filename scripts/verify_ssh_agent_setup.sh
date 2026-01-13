8291b491221754134ebfa54a0b0c77b7d259316d
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-12 16:47:09 -0800

    Update verify_ssh_agent_setup.sh

--- scripts/verify_ssh_agent_setup.sh
+++ scripts/verify_ssh_agent_setup.sh
@@ -9,7 +9,7 @@ source "${HOME}/.genomac-user/scripts/0_initialize_me.sh"
 
 ############################## BEGIN SCRIPT PROPER ##############################
 
-safe_source "${GMU_PREFS_SCRIPTS}/verify_ssh_agent_configuration.sh"
+safe_source "${GMU_PREFS_SCRIPTS}/interactive_configure_1password.sh"
 
 function main() {
   verify_ssh_agent_configuration
