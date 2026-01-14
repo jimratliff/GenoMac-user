d3b9da25f3bdd82f718945f39852d4111c5210ba
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-14 13:55:00 -0800

    Update defaults_detective.sh

--- scripts/defaults_detective.sh
+++ scripts/defaults_detective.sh
@@ -13,7 +13,9 @@ this_script_dir="${this_script_path:h}"
 source "${HOME}/.genomac-user/scripts/0_initialize_me.sh"
 
 DEFAULTS_DETECTIVE_FUNCTIONS_DIR="${this_script_dir}/defaults_detective"
-source "${DEFAULTS_DETECTIVE_FUNCTIONS_DIR}/find_diff_from_setting_change.sh"
+report "this_script_dir: $this_script_dir"
+report "DEFAULTS_DETECTIVE_FUNCTIONS_DIR: $DEFAULTS_DETECTIVE_FUNCTIONS_DIR"
+safe_source "${DEFAULTS_DETECTIVE_FUNCTIONS_DIR}/find_diff_from_setting_change.sh"
 
 report_start_phase 'Begin determining changes to defaults due to change(s) in preferences'
 
