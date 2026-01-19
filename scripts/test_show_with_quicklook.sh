400fd2f57865930d2222213e10d70176a9ae8a3d
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-18 20:54:09 -0800

    Update test_show_with_quicklook.sh

--- scripts/test_show_with_quicklook.sh
+++ scripts/test_show_with_quicklook.sh
@@ -8,6 +8,6 @@ source "${HOME}/.genomac-user/scripts/0_initialize_me.sh"
 
 report_action_taken "I am about to show you a Markdown document."
 
-doc_to_show="${GENOMAC_USER_LOCAL_DOCUMENTATION_DIRECTORY}/test.md"
+doc_to_show="${GENOMAC_USER_DOCS_TO_DISPLAY_DIRECTORY}/test.md"
 
 show_file_using_quicklook "$doc_to_show"
