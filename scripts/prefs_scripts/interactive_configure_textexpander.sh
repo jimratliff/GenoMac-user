3b13f4b0127bddb244b9bc36b92993a94535c818
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-18 20:50:23 -0800

    Update interactive_configure_textexpander.sh

--- scripts/prefs_scripts/interactive_configure_textexpander.sh
+++ scripts/prefs_scripts/interactive_configure_textexpander.sh
@@ -15,7 +15,7 @@ function interactive_configure_textexpander() {
   report_start_phase_standard
   
   launch_app_and_prompt_user_to_act \
-    --show-doc "${GENOMAC_USER_LOCAL_DOCUMENTATION_DIRECTORY}/TextExpander_how_to_configure.md" \
+    --show-doc "${GENOMAC_USER_DOCS_TO_DISPLAY_DIRECTORY}/TextExpander_how_to_configure.md" \
     "$BUNDLE_ID_TEXTEXPANDER" \
     "Follow the instructions in the Quick Look window to log into and configure TextExpander"
     
