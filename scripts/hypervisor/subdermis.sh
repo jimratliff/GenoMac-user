0b28779ef0d4de840e96fc8f6d61c438adbe52fe
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-30 15:02:32 -0800

    Update subdermis.sh

--- scripts/hypervisor/subdermis.sh
+++ scripts/hypervisor/subdermis.sh
@@ -11,7 +11,7 @@ safe_source "${GMU_SETTINGS_SCRIPTS}/perform_basic_user_level_settings.sh"
 safe_source "${GMU_SETTINGS_SCRIPTS}/perform_initial_bootstrap_operations.sh"
 safe_source "${GMU_SETTINGS_SCRIPTS}/perform_stow_dotfiles.sh"
 
-function run_hypervisor() {
+function subdermis() {
 
   report_start_phase_standard
 
