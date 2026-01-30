6dbe1d4233431704970c7a25ed3e82e95bc1942f
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-29 17:01:00 -0800

    Update test_show_with_quicklook.sh

--- scripts/tests/test_show_with_quicklook.sh
+++ scripts/tests/test_show_with_quicklook.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 # Test Showing a doc with Quick Look
 
-source "${HOME}/.genomac-user/scripts/0_initialize_me.sh"
+source "${HOME}/.genomac-user/scripts/0_initialize_me_first.sh"
 
 report_action_taken "I am about to show you a Markdown document."
 
-doc_to_show="${GENOMAC_USER_DOCS_TO_DISPLAY_DIRECTORY}/test.md"
+doc_to_show="${GMU_DOCS_TO_DISPLAY}/test.md"
 
 show_file_using_quicklook "$doc_to_show"
