07f233eb685ad6037405ffe73f1807db60c7900e
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-29 17:03:28 -0800

    Update test_refactor.sh

--- scripts/tests/test_refactor.sh
+++ scripts/tests/test_refactor.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 # Test refactoring
 
-source "${HOME}/.genomac-user/scripts/0_initialize_me.sh"
+source "${HOME}/.genomac-user/scripts/0_initialize_me_first.sh"
 
 report "Hopefully, you’re seeing this because my refactoring was successful!"
