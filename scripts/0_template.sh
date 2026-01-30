346553564677ef6612c84794d96552fc7072f788
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-29 17:02:07 -0800

    Update 0_template.sh

--- scripts/0_template.sh
+++ scripts/0_template.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 # Template for entry-point scripts
 
-source "${HOME}/.genomac-user/scripts/0_initialize_me.sh"
+source "${HOME}/.genomac-user/scripts/0_initialize_me_first.sh"
 
 ############################## BEGIN SCRIPT PROPER ##############################
 
