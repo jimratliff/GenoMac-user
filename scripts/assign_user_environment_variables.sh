7b974b15a60efbd685c3d387025721c816029233
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-31 14:34:47 -0800

    Update assign_user_environment_variables.sh

--- scripts/assign_user_environment_variables.sh
+++ scripts/assign_user_environment_variables.sh
@@ -104,7 +104,6 @@ export_and_report GMU_RESOURCES
 export_and_report GMU_SCRIPTS
 export_and_report GMU_SETTINGS_SCRIPTS
 export_and_report GMU_UTILITIES
-export_and_report HOMEBREW_PREFIX
 export_and_report META_MODIFIER_CHARS
 export_and_report MODIFIERS_KEYBOARD_NAVIGATION
 export_and_report OPTION_CHAR
