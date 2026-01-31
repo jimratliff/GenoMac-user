85ed062b20c434f5130ffa6647b9ce7e8aca7964
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-30 17:25:51 -0800

    Update perform_initial_bootstrap_operations.sh

--- scripts/settings/perform_initial_bootstrap_operations.sh
+++ scripts/settings/perform_initial_bootstrap_operations.sh
@@ -5,6 +5,18 @@ safe_source "${GMU_SETTINGS_SCRIPTS}/bootstrap_finder.sh"
 safe_source "${GMU_SETTINGS_SCRIPTS}/bootstrap_preview_app.sh" 
 safe_source "${GMU_SETTINGS_SCRIPTS}/register_glance_as_quicklook.sh"
 
+function conditionally_perform_initial_bootstrap_operations() {
+  report_start_phase_standard
+
+  run_if_user_has_not_done \
+    --force-logout \
+    "$PERM_BASIC_BOOTSTRAP_OPERATIONS_HAVE_BEEN_PERFORMED" \
+    perform_initial_bootstrap_operations \
+    "Skipping basic bootstrap operations, because they’ve already been performed"
+  
+  report_end_phase_standard
+}
+
 function perform_initial_bootstrap_operations() {
   report_start_phase_standard
 
