828a761df3900eb2141925cdbe93553b6647072c
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-02-12 20:32:46 -0800

    Update set_waterfox_settings.sh

--- scripts/settings/set_waterfox_settings.sh
+++ scripts/settings/set_waterfox_settings.sh
@@ -68,27 +68,21 @@ function install_waterfox_extensions() {
 }
 
 function implement_waterfox_preferences() {
-	# Implement Waterfox preferences idempotently.
+	# Implement Waterfox preferences idempotently by copying idempotently a user.js file
+	# from GenoMac-user/resources/waterfox to an appropriate destination within 
+	# ~/Library/Application Support/Waterfox
 	#
 	# Waterfox’s preferences are implemented by JavaScript `user_pref` commands in
 	# the pref.js file within a user’s profile, e.g.,
 	# '~/Library/Application Support/Waterfox/Profiles/abcdefgh.default-release/prefs.js',
 	# abcdefgh is a string specific to the user’s profile.
-	#
-	# This script implements the desired preferences by, in the initial bootstrap instance,
-	# appending new `user_pref` commands to the existing set (each user_pref of which either
-	# adds to or overrides a prior, default user_pref command).
-	#
-	# The appended block begins with a start_marker comment and ends with an end_marker
-	# comment.
-	#
-	# Subsequent runs of this script detects preexistence of a start_marker … end_marker
-	# snippet and replaces it instead of appending again.
-	#
-	# NOTE: FAIL. The following was intended to change the default search engine to Google,
-	#       but it didn’t work.
-	#         // Search » Default search engine: Google
-    #         user_pref("browser.urlbar.placeholderName", "Google");
+	# However, that file contains the following instructions:
+	#   // If you make changes to this file while the application is running,
+    #   // the changes will be overwritten when the application exits.
+    #   //
+    #   // To change a preference value, you can either:
+    #   // - modify it via the UI (e.g. via about:config in the browser); or
+    #   // - set it within a user.js file in your profile.
 
   report_start_phase_standard
   report_action_taken "Configuring Waterfox preferences"
@@ -96,98 +90,12 @@ function implement_waterfox_preferences() {
   ensure_waterfox_profiles_path_exists
   quit_app_by_bundle_id_if_running "$BUNDLE_ID_WATERFOX"
 
+  local source_path="${GMU_RESOURCES}/waterfox/user.js"
+  
   local profile_dir=$(get_unique_active_Waterfox_profile)
-  local prefs_file="$profile_dir/prefs.js"
+  local destination_path="$profile_dir/user.js"
 
-  if [[ ! -f "$prefs_file" ]]; then
-    report_fail "prefs.js not found at $prefs_file"
-    return 1
-  fi
-
-  # NOTE: Do not change start_marker or end_marker! Later runs will search for exact
-  #       matches of these strings.
-  local start_marker="// >>> GenoMac-user preferences start <<<"
-  local end_marker="// >>> GenoMac-user preferences end <<<"
-
-  local prefs_block
-  prefs_block=$(cat << 'EOF'
-// General » Always check if Waterfox is your default browser? NO
-user_pref("browser.shell.checkDefaultBrowser", false);
-
-// General » Tabs » Open links in tabs instead of new windows? NO!
-user_pref("browser.link.open_newwindow", 2);
-
-// General » Tabs » Ask before closing multiple tabs? NO
-user_pref("browser.tabs.warnOnClose", false);
-
-// General » Tab Context Menu » Show copy all tab urls menu item? YES
-user_pref("browser.tabs.copyallurls", true);
-
-// General » Language and Appearance » Fonts » Default font: Palatino
-user_pref("font.name.serif.x-western", "Palatino");
-
-// General » Language and Appearance » Status Bar » Show Status Bar: YES
-user_pref("browser.statusbar.enabled", true);
-
-// General » Files and Applications » Applications » What should Waterfox do with other files? Save, don't ask
-user_pref("browser.download.always_ask_before_handling_new_types", false);
-
-// General » Browsing » Enable Picture-in-Picture video controls? NO
-user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
-
-// Home » Homepage and new windows » Blank Page
-user_pref("browser.startup.homepage", "chrome://browser/content/blanktab.html");
-
-// Home » New Tabs » Blank Page
-user_pref("browser.newtabpage.enabled", false);
-
-// Privacy & Security » WebRTC peer connection » Do NOT enable WebRTC peer connection
-user_pref("media.peerconnection.enabled", false);
-
-// Privacy & Security » Passwords » Ask to save passwords? NO
-user_pref("signon.rememberSignons", false);
-
-// Privacy & Security » Passwords » Fill usernames and passwords automatically? NO
-user_pref("signon.autofillForms", false);
-
-// Privacy & Security » Passwords » Suggest strong passwords? NO
-user_pref("signon.generation.enabled", false);
-
-// Privacy & Security » Autofill » Do NOT save and fill addresses
-user_pref("extensions.formautofill.addresses.enabled", false);
-
-// Privacy & Security » Autofill » Do NOT save and fill payment methods
-user_pref("extensions.formautofill.creditCards.enabled", false);
-
-// Privacy & Security » Security » Enable HTTPS-Only Mode in all windows
-user_pref("dom.security.https_only_mode", true);
-user_pref("dom.security.https_only_mode_ever_enabled", true);
-
-// Look & Feel » Icons » Show Mac menu icons
-user_pref("userChrome.icon.global_menu.mac", true);
-EOF
-)
-
-  if grep -q "$start_marker" "$prefs_file"; then
-    # Replace existing block
-    local temp_file=$(mktemp)
-    awk -v start="$start_marker" -v end="$end_marker" -v block="$prefs_block" '
-      $0 == start { print; print block; skip=1; next }
-      $0 == end { skip=0 }
-      !skip { print }
-    ' "$prefs_file" > "$temp_file"
-    mv "$temp_file" "$prefs_file"
-    report_action_taken "Updated existing GenoMac preferences block"
-  else
-    # Append new block
-    cat >> "$prefs_file" << EOF
-
-$start_marker
-$prefs_block
-$end_marker
-EOF
-    report_action_taken "Appended new GenoMac preferences block"
-  fi
+  copy_resource_between_local_directories "$source_file" "$dest_file"
 
   report_end_phase_standard
 }
