7c57c3243aedc9802ffefff10845588314fde79d
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-02-16 21:21:08 -0800

    Update interactive_configure_1password.sh

--- scripts/settings/interactive_configure_1password.sh
+++ scripts/settings/interactive_configure_1password.sh
@@ -15,22 +15,19 @@ function conditionally_configure_1Password() {
   #
   # It is assumed that all users want to be authenticated with 1Password.
   # However, each user can choose whether to go to the extra effort to configure 1Password’s SSH agent
-  #
-  # It is assumed that: if a user has configured 1Password, then that user has also authenticated 1Password
   
   report_start_phase_standard
-
-  # Skip if 1Password has been previously configured for this user (even in an earlier session)
-  if test_genomac_user_state "$PERM_1PASSWORD_HAS_BEEN_CONFIGURED"; then
-    report_action_taken "Skipping 1Password configuration, because it’s already been configured and it’s a bootstrapping step"
-    report_end_phase_standard
-    return 0
-  fi
 
   # Conditionally prompt user to authenticate their 1Password account in the 1Password app on the Mac
   run_if_user_has_not_done "$PERM_1PASSWORD_HAS_BEEN_AUTHENTICATED" \
     authenticate_1Password \
     "Skipping authenticating 1Password, because it’s already been authenticated and it’s a bootstrapping step."
+
+  # Conditionally prompt user to configure basic settings for their already-authenticated 1Password
+  run_if_user_has_not_done \
+    "$PERM_1PASSWORD_HAS_BEEN_BASICALLY_CONFIGURED" \
+    interactive_basic_configure_1password \
+    "Skipping basic configuration of 1Password, because you've done that in the past."
 
   # Conditionally prompt user to configure their already-authenticated 1Password
   run_if_user_state "$PERM_1PASSWORD_USER_WANTS_TO_CONFIGURE_SSH_AGENT" \
@@ -53,14 +50,30 @@ function authenticate_1Password() {
   report_end_phase_standard
 }
 
-function configure_authenticated_1Password() {
+
+
+function interactive_basic_configure_1password() {
+  report_start_phase_standard
+  
+  report "Time to configure basic settings for 1Password! I’ll launch it, and open a window with instructions for next steps"
+	
+  launch_app_and_prompt_user_to_act \
+    --show-doc "${GMU_DOCS_TO_DISPLAY}/1Password_how_to_log_in.md" \
+    "$BUNDLE_ID_1PASSWORD" \
+    "Follow the instructions in the Quick Look window to log into your 1Password account in the 1Password app"
+
+  report_end_phase_standard
+}
+
+
+function configure_authenticated_1Password_for_ssh() {
   # Prompt user to configure settings of 1Password
   report_start_phase_standard
   
   report_action_taken "Time to configure 1Password! I'll launch it, and open a window with instructions to follow"
   
   launch_app_and_prompt_user_to_act \
-    --show-doc "${GMU_DOCS_TO_DISPLAY}/1Password_how_to_configure.md" \
+    --show-doc "${GMU_DOCS_TO_DISPLAY}/1Password_how_to_configure_for_ssh.md" \
     "$BUNDLE_ID_1PASSWORD" \
     "Follow the instructions in the Quick Look window to configure 1Password"
 
@@ -71,19 +84,22 @@ function configure_and_verify_authenticated_1Password() {
   # Prompt user to configure settings of 1Password
   report_start_phase_standard
   configure_authenticated_1Password
-  if ! verify_ssh_agent_configuration; then
+  if ! verify_ssh_agent_configuration_for_GitHub; then
     report_fail "The attempt to configure 1Password to SSH authenticate with GitHub has failed ☹️"
     report_end_phase_standard
     return 1
   fi
   
   report success "✅ 1Password successfully configured to SSH authenticate with GitHub"
-  set_genomac_user_state "$PERM_1PASSWORD_HAS_BEEN_CONFIGURED"
+  set_genomac_user_state "$PERM_1PASSWORD_HAS_BEEN_CONFIGURED_FOR_SSH"
   report_end_phase_standard
 }
 
-function verify_ssh_agent_configuration() {
+function verify_ssh_agent_configuration_for_GitHub() {
+  # Tests SSH configuration for authenticating with GitHub via SSH. Returns 0 if pass; 1 if fail
+  
   report_start_phase_standard
+  
   if [[ -z "${SSH_AUTH_SOCK:-}" ]]; then
     report_error "SSH_AUTH_SOCK is not set. Failed: SSH agent not configured"
     report_end_phase_standard
