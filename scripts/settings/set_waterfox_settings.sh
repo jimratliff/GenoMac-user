24928c572fd8aceb1cf884874c7dc70f9094eb10
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-02-18 20:18:44 -0800

    Update set_waterfox_settings.sh

--- scripts/settings/set_waterfox_settings.sh
+++ scripts/settings/set_waterfox_settings.sh
@@ -17,26 +17,7 @@ function conditionally_implement_waterfox_settings_and_install_extensions() {
     "$SESH_WATERFOX_SETTINGS_HAVE_BEEN_IMPLEMENTED" \
     implement_waterfox_preferences \
     "Skipping specifying Waterfox preferences, because they’ve already been specified this session"
-
-  if test_user_state "$PERM_WATERFOX_EXTENSION_YOUTUBE_WANTS_TO_CONFIGURE"; then
-    run_if_user_has_not_done \
-      "$PERM_WATERFOX_EXTENSION_YOUTUBE_HAS_BEEN_CONFIGURED" \
-      waterfox_how_to_configure_youtube_extension \
-      "Skipping configuring Enhanced for YouTube extension for Waterfox, because it’s already been specified in the past"
 	
-  report_end_phase_standard
-}
-
-function interactive_configure_waterfox_youtube_extension() {
-  report_start_phase_standard
-  
-  report "Time to configure the Enhanced for YouTube browser extension for Waterfox! I’ll launch it, and open a window with instructions"
-	
-  launch_app_and_prompt_user_to_act \
-    --show-doc "${GMU_DOCS_TO_DISPLAY}/waterfox_how_to_configure_youtube_extension.md" \
-    "$BUNDLE_ID_WATERFOX" \
-    "Follow the instructions in the Quick Look window to configure Enhancer for YouTube for Waterfox"
-
   report_end_phase_standard
 }
 
@@ -92,7 +73,7 @@ function implement_waterfox_preferences() {
 	# Waterfox’s preferences are implemented by JavaScript `user_pref` commands in
 	# the pref.js file within a user’s profile, e.g.,
 	# '~/Library/Application Support/Waterfox/Profiles/abcdefgh.default-release/prefs.js',
-	# abcdefgh is a string specific to the user’s profile.
+	# where 'abcdefgh' is an arbitrary string specific to the user’s profile.
 	#
 	# However, that file contains the following instructions, which are implemented here:
 	#   // If you make changes to this file while the application is running,
