cf64d064005cd5b03427247393d671e0b97815cf
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-02-23 15:13:13 -0800

    Update interactive_configure_helium.sh

--- scripts/settings/interactive_configure_helium.sh
+++ scripts/settings/interactive_configure_helium.sh
@@ -6,7 +6,12 @@ function conditionally_interactive_configure_helium() {
   run_if_user_has_not_done \
     "$PERM_HELIUM_HAS_BEEN_CONFIGURED" \
     interactive_configure_helium \
-    "Skipping configuring Helium because it’s been in the past"
+    "Skipping configuring Helium because it’s been done in the past"
+
+  run_if_user_has_not_done \
+    "$PERM_HELIUM_EXTENSIONS_HAVE_BEEN_INSTALLED" \
+    interactive_install_helium_extensions \
+    "Skipping installing Helium extensions because this has been done in the past"
 
   report_end_phase_standard
 }
@@ -18,6 +23,17 @@ function interactive_configure_helium() {
     --show-doc "${GMU_DOCS_TO_DISPLAY}/Helium_how_to_configure.md" \
     "$BUNDLE_ID_HELIUM" \
     "Follow the instructions in the Quick Look window to configure Helium"
+    
+  report_end_phase_standard
+}
+
+function interactive_install_helium_extensions() {
+  report_start_phase_standard
+  
+  launch_app_and_prompt_user_to_act \
+    --show-doc "${GMU_DOCS_TO_DISPLAY}/Helium_how_to_install_extensions.md" \
+    "$BUNDLE_ID_HELIUM" \
+    "Follow the instructions in the Quick Look window to install extensions for Helium"
     
   report_end_phase_standard
 }
