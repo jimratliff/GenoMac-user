6aff8c5f0ee8dc47e24fb763f644fcfe2614e87b
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-02-26 16:49:43 -0800

    Update perform_initial_bootstrap_operations.sh

--- scripts/settings/perform_initial_bootstrap_operations.sh
+++ scripts/settings/perform_initial_bootstrap_operations.sh
@@ -34,23 +34,3 @@ function conditionally_perform_initial_bootstrap_operations() {
   
   report_end_phase_standard
 }
-
-function perform_initial_bootstrap_operations() {
-  report_start_phase_standard
-
-  report_action_taken "Performing initial, basic bootstrapping steps"
-
-  # Glance: Register as a QuickLook plug
-  register_glance_as_quicklook
-  
-  # Dock: Define initial configuration of persistent apps
-  bootstrap_dock
-  
-  # Finder: Define initial toolbar
-  bootstrap_finder
-  
-  # Preview.app: Define initial toolbar
-  bootstrap_preview_app
-
-  report_end_phase_standard
-}
