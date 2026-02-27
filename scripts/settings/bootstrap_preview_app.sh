5c3fd1c7cc6a8395a06ea2600cf40ff750e65a9d
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-02-26 18:59:44 -0800

    Update bootstrap_preview_app.sh

--- scripts/settings/bootstrap_preview_app.sh
+++ scripts/settings/bootstrap_preview_app.sh
@@ -7,9 +7,12 @@ function bootstrap_preview_app() {
   
   report_start_phase_standard
   report_action_taken "Bootstrap-only configuration of Preview.app’s toolbar"
+
+  local plist_path
+  local toolbar_key
   
-  local plist_path=$(sandboxed_plist_path_from_domain "$DEFAULTS_DOMAINS_PREVIEW")
-  local toolbar_key="NSToolbar Configuration CommonToolbar_v5.1"
+  plist_path=$(sandboxed_plist_path_from_domain "$DEFAULTS_DOMAINS_PREVIEW")
+  toolbar_key="NSToolbar Configuration CommonToolbar_v5.1"
   
   report_action_taken "Launching and quitting Preview to prepare the plist."
   launch_and_quit_app "$BUNDLE_ID_PREVIEW"
