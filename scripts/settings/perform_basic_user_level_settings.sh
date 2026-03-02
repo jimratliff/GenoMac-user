685e657a1ee1482bcac52f4b57a2abc9ffda8a57
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-03-02 13:47:18 -0800

    Do not enter Mission Control when dragging window to top of screen

--- scripts/settings/perform_basic_user_level_settings.sh
+++ scripts/settings/perform_basic_user_level_settings.sh
@@ -80,7 +80,7 @@ function perform_basic_user_level_settings() {
   
   # Always show Sound in menubar
   report_adjust_setting "Always show Sound in menubar (not only when “active”)"
-  defaults -currentHost write com.apple.controlcenter Sound -int 18;success_or_not
+  defaults -currentHost write com.apple.controlcenter Sound -int 18 ; success_or_not
   
   # Give audible feedback when volume is changed
   report_adjust_setting "Give audible feedback when volume is changed"
@@ -89,23 +89,25 @@ function perform_basic_user_level_settings() {
   
   # Show battery percentage in menubar
   report_adjust_setting "Show battery percentage in menubar"
-  defaults -currentHost write com.apple.controlcenter BatteryShowPercentage -bool true;success_or_not
+  defaults -currentHost write com.apple.controlcenter BatteryShowPercentage -bool true ; success_or_not
   
   report_adjust_setting "Show time with seconds"
-  defaults write com.apple.menuextra.clock ShowSeconds -bool true;success_or_not
+  defaults write com.apple.menuextra.clock ShowSeconds -bool true ; success_or_not
   
   # Show Fast User Switching in menubar as Account Name
   report_action_taken "Show Fast User Switching in menubar only as Account Name"
   report_adjust_setting "1 of 2: userMenuExtraStyle = 1 (Account Name)"
   defaults write NSGlobalDomain userMenuExtraStyle -int 1;success_or_not
   report_adjust_setting "2 of 2: UserSwitcher = 2 (menubar only)"
-  defaults -currentHost write com.apple.controlcenter UserSwitcher -int 2;success_or_not
+  defaults -currentHost write com.apple.controlcenter UserSwitcher -int 2 ; success_or_not
+
+  ###############
   
   # Control Center
   # Add Bluetooth to Control Center to access battery percentages of Bluetooth devices
   # This needs to be tested on laptop
   report_adjust_setting "Add Bluetooth to Control Center to access battery percentages of Bluetooth devices"
-  defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool true;success_or_not
+  defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool true ; success_or_not
   
   # Dock
   set_general_dock_settings
@@ -117,13 +119,16 @@ function perform_basic_user_level_settings() {
   report_action_taken "Implement settings related to Spaces (Mission Control)"
   
   report_adjust_setting "Spaces: Don’t rearrange based on most-recent use"
-  defaults write com.apple.dock mru-spaces -bool false;success_or_not
+  defaults write com.apple.dock mru-spaces -bool false ; success_or_not
   
   report_adjust_setting "Spaces span all displays (no separate space for each monitor)"
-  defaults write com.apple.spaces "spans-displays" -bool "true";success_or_not
+  defaults write com.apple.spaces "spans-displays" -bool "true" ; success_or_not
   
   report_adjust_setting "Do not jump to a new space when switching applications"
-  defaults write NSGlobalDomain AppleSpacesSwitchOnActivate -bool false;success_or_not
+  defaults write NSGlobalDomain AppleSpacesSwitchOnActivate -bool false ; success_or_not
+  
+  report_adjust_setting "Do not enter Mission Control when dragging window to top of screen"
+  defaults write com.apple.dock enterMissionControlByTopWindowDrag -bool false ; success_or_not
   
   # Finder
   set_finder_settings
