9f39fd5702f798d1c8720d4610bd0fe80a1236d6
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-03-02 18:26:39 -0800

    Update set_mission_control_assign_to_options.sh

--- scripts/settings/set_mission_control_assign_to_options.sh
+++ scripts/settings/set_mission_control_assign_to_options.sh
@@ -5,19 +5,47 @@ function implement_mission_control_assign_to_options_for_selected_apps(){
 
   local domain="com.apple.spaces"
 
-  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_1PASSWORD}" "AllSpaces"
-  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_ACTIVITY_MONITOR}" "AllSpaces"
-  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_CALENDAR}" "AllSpaces"
-  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_CONTACTS}" "AllSpaces"
-  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_NOTES}" "AllSpaces"
-  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_REMINDERS}" "AllSpaces"
-  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_STICKIES}" "AllSpaces"
-  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_SYSTEM_SETTINGS}" "AllSpaces"
-  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_TEXTEXPANDER}" "AllSpaces"
+  #  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_1PASSWORD}" "AllSpaces"
+  #  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_ACTIVITY_MONITOR}" "AllSpaces"
+  #  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_CALENDAR}" "AllSpaces"
+  #  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_CONTACTS}" "AllSpaces"
+  #  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_NOTES}" "AllSpaces"
+  #  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_REMINDERS}" "AllSpaces"
+  #  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_STICKIES}" "AllSpaces"
+  #  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_SYSTEM_SETTINGS}" "AllSpaces"
+  #  defaults write "${domain}" app-bindings -dict-add "${BUNDLE_ID_TEXTEXPANDER}" "AllSpaces"
+
+  set_bundle_id_to_AllSpaces "${BUNDLE_ID_1PASSWORD}"
+  set_bundle_id_to_AllSpaces "${BUNDLE_ID_ACTIVITY_MONITOR}"
+  set_bundle_id_to_AllSpaces "${BUNDLE_ID_CALENDAR}"
+  set_bundle_id_to_AllSpaces "${BUNDLE_ID_CONTACTS}"
+  set_bundle_id_to_AllSpaces "${BUNDLE_ID_NOTES}"
+  set_bundle_id_to_AllSpaces "${BUNDLE_ID_REMINDERS}"
+  set_bundle_id_to_AllSpaces "${BUNDLE_ID_STICKIES}"
+  set_bundle_id_to_AllSpaces "${BUNDLE_ID_SYSTEM_SETTINGS}"
+  set_bundle_id_to_AllSpaces "${BUNDLE_ID_TEXTEXPANDER}"
 
   killall Dock
   
   report_end_phase_standard
+}
+
+function set_bundle_id_to_AllSpaces(){
+  # $1: Bundle ID of app to assign to AllSpaces
+  #
+  # Empirically, the required defaults write doesn’t seem to work properly when the bundle_ID is
+  # not fully lowercase: It doesn’t result in the Assign-to option being set to "AllSpaces"
+  # I don't want to change all my BUNDLE_ID_XXXX environment variables to be lowercase,
+  # because I want to keep them faithful to what mdls tells me.
+  # Thus, this function lowercases the BUNDLE_ID before using it.
+
+  local bundle_id
+  local lower_case_bundle_id
+  bundle_id="$1"
+  lower_case_bundle_id="${bundle_id,,}"
+
+  report_adjust_setting "Setting app $bundle_id to show all of its windows on All Desktops"
+  defaults write com.apple.spaces app-bindings -dict-add "${lower_case_bundle_id}" "AllSpaces"
 }
 
 # function conditionally_assign_apps_a_mission_control_assign_to_option() {
