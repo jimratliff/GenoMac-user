51725a0d20cd98c8e1acdbdf74a5804bba4a9034
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-31 22:31:37 -0700

    Create user_attribute_scripts.sh

---
+++ scripts/settings/user_attribute_scripts.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+function transfer_system_scoped_user_attribute_states_to_user_scoped() {
+  # For the current user, transfer system-scoped user-attribute states for this
+  # user to become user-scoped user-attribute states.
+  
+  report_start_phase_standard
+  local short_name
+  local state_prefix
+
+
+  
+  short_name="$(short_name_of_user_from_HOME)"
+  state_prefix=
+
+
+  
+  report_end_phase_standard
+}
