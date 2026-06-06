62ca7e659f2eff34ace04b9f232a45243433ab85
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-06-06 13:40:33 -0700

    Update interactive_ask_initial_questions.sh

--- scripts/settings/interactive_ask_initial_questions.sh
+++ scripts/settings/interactive_ask_initial_questions.sh
@@ -3,6 +3,32 @@
 ############### WIP TODO
 # The below questions will largely/entirely be replaced by “user attributes”
 #
+# USER ATTRIBUTES
+# - sysadmin
+# - developer
+# - dropbox
+# - sync_com
+# - microsoft_word
+#
+# The following are deprecated and implied by `developer`
+# - commit_on_github
+# - authenticate_github_via_1password
+#
+# DEFAULT USER ATTRIBUTES BASED ON USER CLASS
+# - superintendent
+#   - sysadmin
+#   - developer
+#   - dropbox
+# - personal
+#   - dropbox
+# - work
+#   - dropbox
+#   - sync_com
+#   - microsoft_word
+# - auxiliary
+#   - dropbox
+#   - sync_com
+#
 # USER STATES
 # 
 # - Show drives on desktop
