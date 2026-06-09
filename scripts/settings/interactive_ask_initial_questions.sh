fe8cdbb5a56d7b304d6cb087d3657a758ac70e8c
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-06-08 20:40:48 -0700

    Update interactive_ask_initial_questions.sh

--- scripts/settings/interactive_ask_initial_questions.sh
+++ scripts/settings/interactive_ask_initial_questions.sh
@@ -70,64 +70,76 @@
 #   - Obsolete
 #     - PERM_Q_ASKED_WANT_SSH_AUTHENTICATE_GITHUB_USING_1PASSWORD
 
+
 function conditionally_interactive_ask_initial_questions() {
 
-  # Ask questions to the executing user that will guide the remainder of the Hypervisor process.
-  # Typically, the answer to each true/false question depends only on the particular user,
-  # not on the particular session.
-  # Therefore, we ask these questions only once, the first time the user’s preferences are set
-  # by GenoMac-user.
+
 
   report_start_phase_standard
 
-  conditionally_ask_question_and_assign_user_state_if_yes \
-    "$PERM_Q_ASKED_FINDER_SHOW_DRIVES_ON_DESKTOP" \
-    "Skipping asking about showing drives on desktop, because already answered." \
-    "Does this user want to see, on the desktop, the built-in and external drives?" \
-    "$PERM_FINDER_SHOW_DRIVES_ON_DESKTOP"
+	############### Set verbosity of output
 
   conditionally_ask_question_and_assign_user_state_if_yes \
-    "$PERM_Q_ASKED_CONFIGURE_YOUTUBE_ENHANCER_FOR_WATERFOX" \
-    "Skipping asking about Enhancer for YouTube extension for Waterfox, because already answered." \
-    "Does this user want to configure the Enhancer for YouTube browser extension for Waterfox?" \
-    "$PERM_WATERFOX_EXTENSION_YOUTUBE_WANTS_TO_CONFIGURE"
+    "$SESH_Q_ASKED_VERBOSITY" \
+    "Skipping asking about verbosity, because this has already been answered this session." \
+    "Do you want verbose terminal output?" \
+    "$SESH_VERBOSITY_USER_WANTS_IT"
 
-  conditionally_ask_question_and_assign_user_state_if_yes \
-    "$PERM_Q_ASKED_WANT_DROPBOX" \
-    "Skipping asking about using Dropbox, because already answered." \
-    "Will this user sync preferences via Dropbox?" \
-    "$PERM_DROPBOX_USER_WANTS_IT"
-
-  ############### Questions re developing on GitHub
-  #
-  # If user wants to use 1Password to authenticate GitHub, necessarily the user also wants
-  # to make commits on GitHub. Thus, no need to ask that question separately.
-  # Conceivably, though barely, a user could want to make commits on GitHub but doesn’t want
-  # to configure SSH via 1Password.
-
-  conditionally_ask_question_and_assign_user_state_if_yes \
-    "$PERM_Q_ASKED_WANT_SSH_AUTHENTICATE_GITHUB_USING_1PASSWORD" \
-    "Skipping asking about using 1Password to authenticate GitHub, because already answered." \
-    "Will this user want to SSH authenticate GitHub using 1Password?" \
-    "$PERM_1PASSWORD_USER_WANTS_TO_CONFIGURE_SSH_AGENT"
-
-  # TODO: This logic seems wrong. If NO to PERM_1PASSWORD_USER_WANTS_TO_CONFIGURE_SSH_AGENT",
-  #       it will ALWAYS ask PERM_USER_WANTS_TO_COMMIT_ON_GITHUB"?
-  # Fortunately, this is being replaced
-  if test_genomac_user_state "$PERM_1PASSWORD_USER_WANTS_TO_CONFIGURE_SSH_AGENT"; then
-    set_genomac_user_state "$PERM_USER_WANTS_TO_COMMIT_ON_GITHUB"
-  else
-    set_user_state_based_on_yes_no \
-	  "$PERM_USER_WANTS_TO_COMMIT_ON_GITHUB" \
-	  "Will this user want to make commits on GitHub? (If so, will set name/email for Git config)"
-  fi
-  ###############
-
-  conditionally_ask_question_and_assign_user_state_if_yes \
-    "$PERM_Q_ASKED_WANT_MICROSOFT_WORD" \
-    "Skipping asking about using Microsoft Word, because already answered." \
-    "Will this user want to configure Microsoft Word?" \
-    "$PERM_MICROSOFT_WORD_USER_WANTS_IT"
+#   # ############### DEPRECATED QUESTIONS
+#   # Ask questions to the executing user that will guide the remainder of the Hypervisor process.
+#   # Typically, the answer to each true/false question depends only on the particular user,
+#   # not on the particular session.
+#   # Therefore, we ask these questions only once, the first time the user’s preferences are set
+#   # by GenoMac-user.
+# 
+#   conditionally_ask_question_and_assign_user_state_if_yes \
+#     "$PERM_Q_ASKED_FINDER_SHOW_DRIVES_ON_DESKTOP" \
+#     "Skipping asking about showing drives on desktop, because already answered." \
+#     "Does this user want to see, on the desktop, the built-in and external drives?" \
+#     "$PERM_FINDER_SHOW_DRIVES_ON_DESKTOP"
+# 
+#   conditionally_ask_question_and_assign_user_state_if_yes \
+#     "$PERM_Q_ASKED_CONFIGURE_YOUTUBE_ENHANCER_FOR_WATERFOX" \
+#     "Skipping asking about Enhancer for YouTube extension for Waterfox, because already answered." \
+#     "Does this user want to configure the Enhancer for YouTube browser extension for Waterfox?" \
+#     "$PERM_WATERFOX_EXTENSION_YOUTUBE_WANTS_TO_CONFIGURE"
+# 
+#   conditionally_ask_question_and_assign_user_state_if_yes \
+#     "$PERM_Q_ASKED_WANT_DROPBOX" \
+#     "Skipping asking about using Dropbox, because already answered." \
+#     "Will this user sync preferences via Dropbox?" \
+#     "$PERM_DROPBOX_USER_WANTS_IT"
+# 
+#   ############### Questions re developing on GitHub
+#   #
+#   # If user wants to use 1Password to authenticate GitHub, necessarily the user also wants
+#   # to make commits on GitHub. Thus, no need to ask that question separately.
+#   # Conceivably, though barely, a user could want to make commits on GitHub but doesn’t want
+#   # to configure SSH via 1Password.
+# 
+#   conditionally_ask_question_and_assign_user_state_if_yes \
+#     "$PERM_Q_ASKED_WANT_SSH_AUTHENTICATE_GITHUB_USING_1PASSWORD" \
+#     "Skipping asking about using 1Password to authenticate GitHub, because already answered." \
+#     "Will this user want to SSH authenticate GitHub using 1Password?" \
+#     "$PERM_1PASSWORD_USER_WANTS_TO_CONFIGURE_SSH_AGENT"
+# 
+#   # TODO: This logic seems wrong. If NO to PERM_1PASSWORD_USER_WANTS_TO_CONFIGURE_SSH_AGENT",
+#   #       it will ALWAYS ask PERM_USER_WANTS_TO_COMMIT_ON_GITHUB"?
+#   # Fortunately, this is being replaced
+#   if test_genomac_user_state "$PERM_1PASSWORD_USER_WANTS_TO_CONFIGURE_SSH_AGENT"; then
+#     set_genomac_user_state "$PERM_USER_WANTS_TO_COMMIT_ON_GITHUB"
+#   else
+#     set_user_state_based_on_yes_no \
+# 	  "$PERM_USER_WANTS_TO_COMMIT_ON_GITHUB" \
+# 	  "Will this user want to make commits on GitHub? (If so, will set name/email for Git config)"
+#   fi
+#   ###############
+# 
+#   conditionally_ask_question_and_assign_user_state_if_yes \
+#     "$PERM_Q_ASKED_WANT_MICROSOFT_WORD" \
+#     "Skipping asking about using Microsoft Word, because already answered." \
+#     "Will this user want to configure Microsoft Word?" \
+#     "$PERM_MICROSOFT_WORD_USER_WANTS_IT"
 
   report_end_phase_standard
 }
