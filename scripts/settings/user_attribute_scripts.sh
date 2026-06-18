b86dfbaff5e2e25acb7c49210528f2d544eec0db
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-06-17 22:06:11 -0700

    Update user_attribute_scripts.sh

--- scripts/settings/user_attribute_scripts.sh
+++ scripts/settings/user_attribute_scripts.sh
@@ -11,6 +11,7 @@ function set_user_preferences_for_attribute() {
   case "$attribute_name" in
     touchid_*)
       report_action_taken_to_log "Setting preferences for attribute: touchid_"
+      set_genomac_user_state "$SESH_TOUCH_ID_USER_WANTS_IT"
       set_SESH_state_for_user_touch_id_choice_from_attribute_name "$attribute_name"
       ;;
     "chessplayer")
