f7277df2762e794b4b6a210e9716bc2f02b6b877
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-06-18 15:38:52 -0700

    Update user_attribute_scripts.sh

--- scripts/settings/user_attribute_scripts.sh
+++ scripts/settings/user_attribute_scripts.sh
@@ -12,7 +12,7 @@ function set_user_preferences_for_attribute() {
     "${USER_ATTRIBUTE_TOUCH_ID_PREFIX}"*)
       # touchid attributes are encoded with a suffix signalling which finger that user
       # is assigned. Thus, we match the attribute name only against its common prefix.
-      report_action_taken_to_log "Setting preferences for attribute: ${USER_ATTRIBUTE_TOUCH_ID_PREFIX}"
+      report_action_taken_to_log "Setting preferences for attribute: ${USER_ATTRIBUTE_TOUCH_ID_PREFIX}*"
       set_genomac_user_state "$SESH_TOUCH_ID_USER_WANTS_IT"
       set_SESH_state_for_user_touch_id_choice_from_attribute_name "$attribute_name"
       ;;
