fab704593a0667f95801b8bbd7906fc2d6762b1a
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-06-17 22:09:45 -0700

    Update interactive_configure_touch_id.sh

--- scripts/settings/interactive_configure_touch_id.sh
+++ scripts/settings/interactive_configure_touch_id.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env zsh
 
+function conditionally_interactive_configure_touch_ID() {
+  # Template for a Zsh function in Project GenoMac
+  report_start_phase_standard
 
+  if ! test_genomac_user_state "$SESH_TOUCH_ID_USER_WANTS_IT"; then
+    report_to_log "Skipping configuring Touch ID because the user doesn’t want it."
+    return 0
+  fi
+    
+  run_if_user_has_not_done "$PERM_TOUCH_ID_HAS_BEEN_CONFIGURED" \
+    interactive_configure_touch_ID \
+    "Skipping configuring Touch ID because this has already been done."
+  
+  report_end_phase_standard
+}
+
+function interactive_configure_touch_ID() {
+  ############### TODO WIP
+  # Template for a Zsh function in Project GenoMac
+  report_start_phase_standard
+  report_end_phase_standard
+}
 
 function set_SESH_state_for_user_touch_id_choice_from_attribute_name() {
   # Sets SESH state to record user’s Touch ID finger choice.
