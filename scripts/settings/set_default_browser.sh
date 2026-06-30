1c73e8ea1344e1b30e293bb3145731457abf2d98
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-06-29 21:26:35 -0700

    Update set_default_browser.sh

--- scripts/settings/set_default_browser.sh
+++ scripts/settings/set_default_browser.sh
@@ -4,6 +4,15 @@ function set_default_browser() {
   # Sets the user’s chosen browser as the default browser. (See "$chosen_browser_id".)
   # Specify the browser’s Bundle Identifier (CFBundleIdentifier) as the argument.
   #
+  # WARNING: The latest published release (v1.0.18) as of 6/29/2026 is NOT compatible
+  #          with home directories that are not on the startup volume. (Thus, it is not
+  #          compatible with almost every user of Project GenoMac.) See Issue #23,
+  #          “default-browser assumes home directory is /Users/<username>, which fails
+  #          for relocated macOS home directories #23”
+  #          https://github.com/macadmins/default-browser/issues/23
+  #
+  #          I have forked this repo and applied a conjected fix.
+  #
   # Relies on the `default-browser` CLI tool by macadmins, available on GitHub but not
   # (as of 1/4/2026) from Homebrew. It is installed by GenoMac-system.
   # https://github.com/macadmins/default-browser
@@ -34,6 +43,11 @@ function set_default_browser() {
   # local chosen_browser_id="$BUNDLE_ID_HELIUM"
   # local chosen_browser_id="$BUNDLE_ID_SAFARI"
   local chosen_browser_id="$BUNDLE_ID_WATERFOX"
+
+  if ! user_home_directory_is_on_startup_volume; then
+    report_fail "default-browser isn’t compatible with user directories that aren’t on the startup volume"
+    return 1
+  fi
   
   report_action_taken "Set default browser to $chosen_browser_id"
   "$path_to_installed_default_browser_utility" --identifier "$chosen_browser_id" ; success_or_not
