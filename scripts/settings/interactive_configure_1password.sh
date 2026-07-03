048694e59567d673e4869bf831a635bb7a27f46b
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-03 15:04:16 -0700

    Update interactive_configure_1password.sh

--- scripts/settings/interactive_configure_1password.sh
+++ scripts/settings/interactive_configure_1password.sh
@@ -48,6 +48,7 @@ function conditionally_configure_1Password() {
   if ! test_genomac_user_state "$SESH_1PASSWORD_USER_WANTS_TO_CONFIGURE_SSH_AGENT"; then
     report_action_taken_to_log "Skipping configuring 1Password for SSH with GitHub, because it’s not desired"
   else
+    ############### BEGIN: REFACTOR AFTER 1PASSWORD SSH AGENT IS FIXED TO WORK WITH USERS ON A NON-STARTUP VOLUME ###############
     if user_home_directory_is_on_startup_volume; then
       run_if_user_has_not_done \
         "$PERM_1PASSWORD_HAS_BEEN_CONFIGURED_FOR_SSH" \
@@ -56,6 +57,7 @@ function conditionally_configure_1Password() {
 	  else
 	    report_warning "Skipping configuring 1Password for SSH with GitHub, because 1Password isn’t compatible with users on a non-startup volume."
     fi
+	############### END: REFACTOR AFTER 1PASSWORD SSH AGENT IS FIXED TO WORK WITH USERS ON A NON-STARTUP VOLUME ###############
   fi
   
   report_end_phase_standard
