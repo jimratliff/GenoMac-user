1c4b2a92237f92862042b57f1417f92ea2fc0207
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-09 10:24:15 -0700

    Update make_development_clones.sh

--- scripts/installations/make_development_clones.sh
+++ scripts/installations/make_development_clones.sh
@@ -9,11 +9,12 @@ function conditionally_clone_GenoMac_repos_for_development() {
     report_to_log "Skipping making additional development clones of GenoMac repos,${NEWLINE}because user doesn’t want to develop Project GenoMac."
   fi
 
-  if is_onepassword_ssh_agent_unavailable_for_this_user; then
-    report_warning "Skipping making dev clones of GenoMac repos, because 1Password SSH Agent isn’t compatible with users on a non-startup volume."
-    report_end_phase_standard
-    return 0
-  fi
+# The following is DEPRECATED
+#   if is_onepassword_ssh_agent_unavailable_for_this_user; then
+#     report_warning "Skipping making dev clones of GenoMac repos, because 1Password SSH Agent isn’t compatible with users on a non-startup volume."
+#     report_end_phase_standard
+#     return 0
+#   fi
 
   run_if_user_has_not_done "$PERM_GENOMAC_DEV_CLONES_HAVE_BEEN_CREATED" \
     make_additional_dev_clones_of_genomac_repos \
@@ -44,10 +45,11 @@ function make_additional_dev_clones_of_genomac_repos() {
   local local_repo_dir_name
   local local_repo_dir
 
-  if is_onepassword_ssh_agent_unavailable_for_this_user; then
-    report_fail "PROGRAMMER ERROR: 1Password SSH Agent isn’t compatible with users on a non-startup volume.${NEWLINE}This point shouldn’t be reached."
-    return 1
-  fi
+# The following is DEPRECATED
+#   if is_onepassword_ssh_agent_unavailable_for_this_user; then
+#     report_fail "PROGRAMMER ERROR: 1Password SSH Agent isn’t compatible with users on a non-startup volume.${NEWLINE}This point shouldn’t be reached."
+#     return 1
+#   fi
 
   create_repositories_directory_for_developers
   
