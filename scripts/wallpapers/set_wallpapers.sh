3dd78d774a1b8b43b0f9398966c366062c65aae1
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-09 10:43:18 -0700

    Update set_wallpapers.sh

--- scripts/wallpapers/set_wallpapers.sh
+++ scripts/wallpapers/set_wallpapers.sh
@@ -1,2 +1,25 @@
 #!/usr/bin/env zsh
 
+function conditionally_set_wallpapers_for_all_spaces() {
+  # Template for a Zsh function in Project GenoMac
+  report_start_phase_standard
+  report_end_phase_standard
+}
+
+function set_wallpapers_for_all_spaces() {
+  # Template for a Zsh function in Project GenoMac
+  report_start_phase_standard
+  report_end_phase_standard
+}
+
+function set_all_displays_of_current_space_to_image_at_path() {
+  # Template for a Zsh function in Project GenoMac
+  report_start_phase_standard
+  report_end_phase_standard
+}
+
+function move_to_mission_control_space_n() {
+  # Template for a Zsh function in Project GenoMac
+  report_start_phase_standard
+  report_end_phase_standard
+}
