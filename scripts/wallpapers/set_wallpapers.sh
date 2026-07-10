b9271d931ee2e52b322ff15176b52af8b9db5df4
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-09 18:26:16 -0700

    Update set_wallpapers.sh

--- scripts/wallpapers/set_wallpapers.sh
+++ scripts/wallpapers/set_wallpapers.sh
@@ -54,11 +54,36 @@ function set_all_displays_of_current_mission_control_space_to_image_at_path() {
 }
 
 function move_to_mission_control_space_n() {
-  # Template for a Zsh function in Project GenoMac
+  # Moves to desired Mission Control Space, specified by its number 1–16
+  #
+  # Relies on assumed already-implemented Hotkey assignments:
+  #  1: ⌃⌥⌘1
+  #  2: ⌃⌥⌘2
+  #  …
+  #  9: ⌃⌥⌘9
+  # 10: ⌃⌥⌘0
+  # 11: ⌃⌥⌘F1
+  # …
+  # 16: ⌃⌥⌘F6
+  
   report_start_phase_standard
   local -i number_of_space_to_which_to_move=${1:MISSING number of space to move to}
+
+  local key_code_for_requested_mission_control_space
+  local key_code_from_mission_control_space_number
+
+  key_code_from_mission_control_space_number=(18 19 20 21 13 22 26 28 25 29 122 120 99 118 96 97)
   
-  ############### TODO WIP
+  key_code_for_requested_mission_control_space=${key_code_from_mission_control_space_number[$number_of_space_to_which_to_move]}
+
+  osascript "$key_code_for_requested_mission_control_space" <<'APPLESCRIPT'
+  on run argv
+    set key_code_for_requested_mission_control_space to item 1 of argv
+    tell application "System Events"
+      key code key_code_for_requested_mission_control_space using {control down, option down, command down}
+    end tell
+  end run
+  APPLESCRIPT
   
   report_end_phase_standard
 }
