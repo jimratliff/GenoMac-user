baa16d62686c5187f9818bc80059e31032a3fd3c
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-17 21:01:36 -0700

    Create define_apps_for_dock()

--- scripts/settings/bootstrap_dock.sh
+++ scripts/settings/bootstrap_dock.sh
@@ -3,45 +3,21 @@
 # Define Dock persistent-app items
 #   Does not alter anything for non-persistent apps.
 #   Does not alter the defaults for the Downloads folder and the Trash Can on the furthest right-hand side of the Dock.
-# Each app in APPS_FOR_DOCK is referenced by its full path.
-# NOTE, IN PARTICULAR, for Apple’s own apps:
-# - Except for Safari, an Apple app (a) *appears* to live in /Applications (or /Applications/Utilities)
-#   but in fact (b) it actually lives in /System/Applications (or, respectively, /System/Applications/Utilities).
-# - It is necessary to specify an Apple app by its *actual* path, not its apparent path.
-# - Safari is exceptional:
-#   - /Application/Safari.app is symlinked to /System/Cryptexes/App/System/Applications/Safari.app
-#   - Presumably that is the path that must be specified to add Safari to the Dock, though I haven’t tested it (but
-#     have seen evidence to support this).
-
-# Original, before refactoring
-# APPS_FOR_DOCK=(
-#   "/System/Applications/System Settings.app"
-#   "Applications/1Password.app"
-#   # "Applications/TextExpander.app"
-#   "Applications/Waterfox.app"
-#   "Applications/Helium.app"
-#   "Applications/Raindrop.io.app"
-#   "Applications/Obsidian.app"
-#   "Applications/Zed.app"
-#   "/System/Applications/Utilities/Activity Monitor.app"
-#   # "/System/Applications/Utilities/Terminal.app"
-#   "Applications/iTerm.app"
-# )
-
-APPS_FOR_DOCK=(
-  "/System/Applications/System Settings.app"
-  "/Applications/1Password.app"
-  "/Applications/Waterfox.app"
-  "/Applications/Helium.app"
-  "/Applications/Raindrop.io.app"
-  "/Applications/Obsidian.app"
-  "/Applications/Zed.app"
-  "/System/Applications/Utilities/Activity Monitor.app"
-  # "/System/Applications/Utilities/Terminal.app"
-  "/Applications/iTerm.app"
-)
 
 function define_apps_for_dock() {
+  # Compile and order apps to populate the Dock depending on user’s attributes.
+  #
+  # Each app in apps_for_dock is referenced by its full path.
+  # NOTE, IN PARTICULAR, for Apple’s own apps:
+  # - Except for Safari, an Apple app (a) *appears* to live in /Applications (or /Applications/Utilities)
+  #   but in fact (b) it actually lives in /System/Applications (or, respectively, /System/Applications/Utilities).
+  # - It is necessary to specify an Apple app by its *actual* path, not its apparent path.
+  # - Safari is exceptional:
+  #   - /Applications/Safari.app is symlinked to /System/Cryptexes/App/System/Applications/Safari.app
+  #   - Presumably that is the path that must be specified to add Safari to the Dock, though I haven’t tested it (but
+  #     have seen evidence to support this).
+  
+  report_start_phase_standard
 
   # Define actual paths to apps: Apple
   local actual_path_to_app_Activity_Monitor="/System/Applications/Utilities/Activity Monitor.app"
@@ -54,24 +30,115 @@ function define_apps_for_dock() {
   # Define actual paths to apps: non-Apple
   local actual_path_to_app_1Password="/Applications/1Password.app"
   local actual_path_to_app_Helium="/Applications/Helium.app"
+  local actual_path_to_app_HIARCS_Chess_Explorer_Pro="/Applications/HIARCS Chess Explorer Pro.app"
   local actual_path_to_app_iTerm="/Applications/iTerm.app"
+  local actual_path_to_app_Microsoft_Word="/Applications/Microsoft Word.app"
   local actual_path_to_app_Obsidian="/Applications/Obsidian.app"
   local actual_path_to_app_Raindrop_io="/Applications/Raindrop.io.app"
   local actual_path_to_app_Waterfox="/Applications/Waterfox.app"
   local actual_path_to_app_Zed="/Applications/Zed.app"
-  
+
+  # By default, 1Password appears as the first item on each user’s Dock.
   local -a apps_for_dock=(
-    "/System/Applications/System Settings.app"
-    "/Applications/1Password.app"
+    "$actual_path_to_app_1Password"
   )
 
-  # Append dynamically as needed.
-  apps_for_dock+=(
-    "/Applications/Waterfox.app"
-  )
+  # Compiles attributes that will guide Dock arrangement
 
-  # Print one array element per line.
+  local -i is_bare_bones=0 # false
+  if test_genomac_user_state "$SESH_USER_WANTS_ONLY_BAREBONES_CONFIG"; then
+    is_bare_bones=1
+  fi
+
+  local -i is_chessplayer=0 # false
+  if test_genomac_user_state "$USER_ATTRIBUTE_CHESSPLAYER"; then
+    is_chessplayer=1
+  fi
+
+  local -i is_developer=0 # false
+  if test_genomac_user_state "$USER_ATTRIBUTE_DEVELOPER" || 
+     test_genomac_user_state "$USER_ATTRIBUTE_GENOMAC_DEVELOPER"
+  then
+    is_developer=1
+  fi
+
+  local -i is_emailer=0 # false
+  if test_genomac_user_state "$USER_ATTRIBUTE_EMAILER"; then
+    is_emailer=1
+  fi
+
+  local -i is_mac_admin=0 # false
+  if test_genomac_user_state "$USER_ATTRIBUTE_MAC_ADMIN"; then
+    is_mac_admin=1
+  fi
+
+  local -i is_switcher=0 # false
+  if test_genomac_user_state "$USER_ATTRIBUTE_SWITCHER"; then
+    is_switcher=1
+  fi
+
+  # Implement Dock arrangement
+
+  # Mail.app
+  if (( is_emailer )); then
+    apps_for_dock+=( "$actual_path_to_app_Mail_app" )
+  fi
+
+  # Waterfox and Helium (which empirically rise and fall together, adjacent in the Dock)
+  if (( ! is_bare_bones )); then
+    apps_for_dock+=( 
+      "$actual_path_to_app_Waterfox" 
+      "$actual_path_to_app_Helium"
+    )
+  else
+    apps_for_dock+=( "$actual_path_to_app_Safari" )
+  fi
+
+  # Raindrop.io
+  if test_genomac_user_state "$USER_ATTRIBUTE_RAINDROP_IO"; then
+    apps_for_dock+=( "$actual_path_to_app_Raindrop_io" )
+  fi
+
+  # Obsidian
+  if test_genomac_user_state "$USER_ATTRIBUTE_OBSIDIAN_USER"; then
+    apps_for_dock+=( "$actual_path_to_app_Obsidian" )
+  fi
+
+  # Microsoft Word
+  if test_genomac_user_state "$USER_ATTRIBUTE_MICROSOFT_WORD"; then
+    apps_for_dock+=( "$actual_path_to_app_Microsoft_Word" )
+  fi
+
+  # HIARCS Chess Explorer Pro
+  if (( is_chessplayer )); then
+    apps_for_dock+=( "$actual_path_to_app_HIARCS_Chess_Explorer_Pro" )
+  fi
+
+  # iTerm and Zed
+  if (( is_developer )); then
+    apps_for_dock+=( 
+      "$actual_path_to_app_iTerm" 
+      "$actual_path_to_app_Zed"
+    )
+  fi
+
+  # System Settings is unconditional
+  apps_for_dock+=( "$actual_path_to_app_System_Settings" )
+
+  # Activity Monitor
+  if (( is_developer || is_mac_admin )); then
+    apps_for_dock+=( "$actual_path_to_app_Activity_Monitor" )
+  fi
+  
+  # Disk Utility
+  if (( is_mac_admin || is_switcher )); then
+    apps_for_dock+=( "$actual_path_to_app_Disk_Utility" )
+  fi
+
+  # Return array by printing one array element per line.
   print -r -l -- "${apps_for_dock[@]}"
+
+  report_end_phase_standard
 }
 
 function bootstrap_dock() {
