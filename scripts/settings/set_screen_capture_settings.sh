b6f7ca443e95c385f339818a92d19999d4ed0a06
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-19 22:20:57 -0700

    Update set_screen_capture_settings.sh

--- scripts/settings/set_screen_capture_settings.sh
+++ scripts/settings/set_screen_capture_settings.sh
@@ -5,36 +5,62 @@ function set_screen_capture_settings() {
   report_start_phase_standard
   report_action_taken "Implement settings related to Screen Capture"
 
-  report_warning "set_screen_capture_settings() needs to be bifurcated to deal with Dropbox screenshot destinations"
+  # For barebones settings, Screenshots is created at root of user’s home directory
+  # This will be later overruled for users who want their Screenshot directory within their Dropbox.
+  local path_for_screen_capture_result="$HOME/Screenshots"
+  local permissions_for_screenshot_directory="700"
+  set_screen_capture_destination "$path_for_screen_capture_result" "$permissions_for_screenshot_directory"
   
-  # NOTE: Setting the location should be separated from the other screen-capture preferences because this
-  #       would be user-specific
-  path_for_screen_capture_result="$HOME/Screenshots"
-  report_adjust_setting "1 of 7: Create screen-capture destination directory if necessary"
-  mkdir -p "$path_for_screen_capture_result";success_or_not
-  
-  report_adjust_setting "2 of 7: Enforce appropriate permissions (700) on screen-capture directory"
-  # The 700 permissions are appropriate because this location in a user’s home directory.
-  chmod 700 "$path_for_screen_capture_result";success_or_not
-  
-  report_adjust_setting "3 of 7: Assign path to screen-capture destination"
-  defaults write com.apple.screencapture location -string "$path_for_screen_capture_result";success_or_not
-  report_adjust_setting "4 of 7: Assign path to previous screen-capture destination"
-  # Per-Claude: By storing into `location-last` your most-recent location, the Screenshot app can populate
-  # the “Other Location…” when using ⌘⇧5. It’s a history/convenience value so the system can offer quick access
-  # to other recently used locations.
-  defaults write com.apple.screencapture location-last -string "$path_for_screen_capture_result";success_or_not
-  
-  report_adjust_setting "5 of 7: Specify that target is a file"
+  report_adjust_setting "Screenshot: Specify that target is a file"
   # Needed only when overriding an assignment to clipboard or an app (e.g., Mail, Preview)
-  defaults write com.apple.screencapture target -string "file";success_or_not
+  defaults write com.apple.screencapture target -string "file" ; success_or_not
   
-  report_adjust_setting "6 of 7: Show floating thumbnail (reinforces default)"
-  defaults write com.apple.screencapture show-thumbnail -bool true;success_or_not
+  report_adjust_setting "Screenshot: Show floating thumbnail (reinforces default)"
+  defaults write com.apple.screencapture show-thumbnail -bool true ; success_or_not
   
-  report_adjust_setting "7 of 7: Disable the drop shadow on screenshots"
-  defaults write com.apple.screencapture disable-shadow -bool "true";success_or_not
+  report_adjust_setting "Screenshot: Disable the drop shadow on screenshots"
+  defaults write com.apple.screencapture disable-shadow -bool "true" ; success_or_not
   
   report_end_phase_standard
 
+}
+
+function set_screen_capture_destination_for_Dropbox_user() {
+
+############### TODO WIP
+
+  # Template for a Zsh function in Project GenoMac
+  report_start_phase_standard
+  report_end_phase_standard
+}
+
+function set_screen_capture_destination() {
+  # Sets path to screenshot folder, creating it if necessary, and enforce permissions.
+  #
+  # Conventional permissions:
+  # - When at root of home directory: 700
+  # - When in Dropbox: 755
+  
+  report_start_phase_standard
+  local screen_capture_destination_path="${1:?MISSING screen-capture destination path}"
+  local permissions="${2:?MISSING numeric permissions}"
+
+  report_adjust_setting "Screenshot: Setting destination to: ${screen_capture_destination_path}"
+
+  report_to_log "Create screen-capture destination directory, if necessary, at $screen_capture_destination_path"
+  mkdir -p "$screen_capture_destination_path"
+
+  report_to_log "Set permissions (${permissions}) on screen-capture destination directory"
+  chmod "$permissions" "$screen_capture_destination_path"
+
+  report_to_log "Assign path to screen-capture directory"
+  defaults write com.apple.screencapture location -string "$screen_capture_destination_path"
+
+  # Store destination into `location-last`, so Screenshot app can populate the “Other Location…” when using ⌘5.
+  # It’s a history/convenience value so the system can offer quick access to other recently used locations.
+  report_to_log "Write path to location-last."
+  defaults write com.apple.screencapture location-last -string "$screen_capture_destination_path"
+
+  success_or_not
+  report_end_phase_standard
 }
