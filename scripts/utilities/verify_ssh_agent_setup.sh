0e5b095cc2e52ee77d839ed4ac1a0a75929205fd
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-24 11:58:41 -0700

    Update verify_ssh_agent_setup.sh

--- scripts/utilities/verify_ssh_agent_setup.sh
+++ scripts/utilities/verify_ssh_agent_setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 
+# Fail early on unset variables or command failure
+set -euo pipefail
+
+source "${HOME}/.genomac-user/scripts/0_initialize_me_first.sh"
+
 safe_source "${GMU_SETTINGS_SCRIPTS}/interactive_configure_1password.sh"
 
 function main() {
