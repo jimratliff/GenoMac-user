fa45bc49668d17495d6048282dede81c3e9b8b87
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-24 11:59:15 -0700

    Create re-deploy_wallpapers_setup.sh

---
+++ scripts/utilities/re-deploy_wallpapers_setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+# Fail early on unset variables or command failure
+set -euo pipefail
+
+source "${HOME}/.genomac-user/scripts/0_initialize_me_first.sh"
+
+safe_source "${GMU_SETTINGS_SCRIPTS}/interactive_set_permissions.sh"
+safe_source "${GMU_SETTINGS_SCRIPTS}/set_wallpapers.sh"
+
+function main() {
+  set_wallpapers_for_all_spaces
+}
+
+main
