1a2f25137c540342c48a860a0b062a46f13c8858
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-08-20 14:58:52 -0700

    Deprecate test for incompatibility with users not on startup volume

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,35 +1,5 @@
 #!/usr/bin/env zsh
 
-function is_functionality_incompatible_with_user_because_not_on_startup_volume() {
-  # Returns 0 if both:
-  # (a) supplied environment variable indicates a particular incompatibility with
-  #     users whose home directories aren’t on the startup volume still exists
-  # (b) this user’s home directory isn’t on the startup volume
-  #
-  # If supplied argument is "true", then the incompatibility still exists.
-  
-  report_start_phase_standard
-  
-  local incompatibility_flag_string="${1:?MISSING incompatibility_flag_string}"
-
-  # If the bug has been fixed, the functionality is available regardless of home-directory location.
-  if [[ "${incompatibility_flag_string}" != "true" ]]; then
-    report_end_phase_standard
-    return 1
-  fi
-
-  # If the user's home directory is on the startup volume, the user isn’t affected by the bug.
-  if user_home_directory_is_on_startup_volume; then
-    report_end_phase_standard
-    return 1
-  fi
-
-  # Bug still exists and this user's home directory is relocated.
-  report_end_phase_standard
-  return 0
-}
-
-
 
 ############### BELOW THIS POINT IS DEPRECATED ###############
 
@@ -53,4 +23,33 @@ function is_functionality_incompatible_with_user_because_not_on_startup_volume()
 # 
 #   is_functionality_incompatible_with_user_because_not_on_startup_volume \
 #     "$DEFAULT_BROWSER_UTILITY_STILL_INCOMPATIBLE_WITH_RELOCATED_HOME_DIRECTORIES" || return $?
+# }
+
+# function is_functionality_incompatible_with_user_because_not_on_startup_volume() {
+#   # Returns 0 if both:
+#   # (a) supplied environment variable indicates a particular incompatibility with
+#   #     users whose home directories aren’t on the startup volume still exists
+#   # (b) this user’s home directory isn’t on the startup volume
+#   #
+#   # If supplied argument is "true", then the incompatibility still exists.
+#   
+#   report_start_phase_standard
+#   
+#   local incompatibility_flag_string="${1:?MISSING incompatibility_flag_string}"
+# 
+#   # If the bug has been fixed, the functionality is available regardless of home-directory location.
+#   if [[ "${incompatibility_flag_string}" != "true" ]]; then
+#     report_end_phase_standard
+#     return 1
+#   fi
+# 
+#   # If the user's home directory is on the startup volume, the user isn’t affected by the bug.
+#   if user_home_directory_is_on_startup_volume; then
+#     report_end_phase_standard
+#     return 1
+#   fi
+# 
+#   # Bug still exists and this user's home directory is relocated.
+#   report_end_phase_standard
+#   return 0
 # }
