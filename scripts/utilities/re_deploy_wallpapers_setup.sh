24a6449d5b322c00cd0db90a2cf65c74aa36be65
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-08-28 19:42:19 -0700

    Update re_deploy_wallpapers_setup.sh

--- scripts/utilities/re_deploy_wallpapers_setup.sh
+++ scripts/utilities/re_deploy_wallpapers_setup.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 
 source "${HOME}/.genomac-user/scripts/0_initialize_me_first.sh"
 
-# safe_source "${GMU_SETTINGS_SCRIPTS}/interactive_set_permissions.sh"
 safe_source "${GMU_SETTINGS_SCRIPTS}/set_wallpapers.sh"
 
 function main() {
