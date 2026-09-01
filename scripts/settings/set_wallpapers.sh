d4ffb4492dc6ef8d77d5bdb59b31dddddd0ada29
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-01 13:08:00 -0700

    Update set_wallpapers.sh

--- scripts/settings/set_wallpapers.sh
+++ scripts/settings/set_wallpapers.sh
@@ -90,7 +90,7 @@ function set_wallpapers_for_all_spaces() {
   # Different Spaces can receive different wallpapers.
   report_start_phase_standard
   local wallpaper_path
-  local -i number_of_current_space
+  local -i space_number
 
   report_action_taken "Assign specified wallpaper to each Mission Control Space."
 
@@ -98,134 +98,20 @@ function set_wallpapers_for_all_spaces() {
   # Space to Space
   interactive_ensure_terminal_has_accessibility     # scripts/settings/interactive_set_permissions.sh
 
-  for (( number_of_current_space=1; number_of_current_space <= MAXIMUM_NUMBER_OF_MISSION_CONTROL_SPACES; ++number_of_current_space )); do
-    move_to_mission_control_space_n "$number_of_current_space"
-    wallpaper_path="$(get_path_to_wallpaper_for_mission_control_space_n "$number_of_current_space")"
-
-    report_to_log "Space ${number_of_current_space} path: ${wallpaper_path}"
-  
+  for (( space_number=1; space_number <= MAXIMUM_NUMBER_OF_MISSION_CONTROL_SPACES; ++space_number )); do
+    move_to_mission_control_space_by_number "$space_number"
+    wallpaper_path="$(get_path_to_wallpaper_for_mission_control_space_by_number "$space_number")"
+    report_to_log "Space ${space_number} path: ${wallpaper_path}"
     set_all_displays_of_current_mission_control_space_to_image_at_path "$wallpaper_path"
   done
 
   # Return to Space #1
-  move_to_mission_control_space_n 1
+  move_to_mission_control_space_by_number 1
   
   report_end_phase_standard
 }
 
-function set_all_displays_of_current_mission_control_space_to_image_at_path() {
-  # Sets wallpaper of all displays of current Mission Control Space to image at supplied path.
-  report_start_phase_standard
-  local wallpaper_path="${1:?MISSING wallpaper_path}"
-
-  local sleeptime_to_allow_deployment_of_wallpapers_to_all_desktop_in_given_space=1
-
-  report_to_log "Current wallpaper path:${NEWLINE}${wallpaper_path}"
-
-  osascript - "$wallpaper_path" <<'APPLESCRIPT'
-on run argv
-  set wallpaper_path to item 1 of argv
-  tell application "System Events"
-    tell every desktop to set picture to wallpaper_path
-  end tell
-end run
-APPLESCRIPT
-
-  sleep "$sleeptime_to_allow_deployment_of_wallpapers_to_all_desktop_in_given_space"
-  report_end_phase_standard
-}
-
-function move_to_mission_control_space_n() {
-  # Moves to desired Mission Control Space, specified by its number 1–16
-  #
-  # Relies on assumed already-implemented Hotkey assignments:
-  #  1: ⌃⌥⌘1
-  #  2: ⌃⌥⌘2
-  #  …
-  #  9: ⌃⌥⌘9
-  # 10: ⌃⌥⌘0
-  # 11: ⌃⌥⌘F1
-  # …
-  # 16: ⌃⌥⌘F6
-  
-  report_start_phase "Entering move_to_mission_control_space_n : $*"
-  
-  local sleeptime_to_allow_for_navigation_to_new_space=1
-  local -i number_of_space_to_which_to_move
-  number_of_space_to_which_to_move="${1:?MISSING number of space to move to}"
-
-  local key_code_for_requested_mission_control_space
-  local -a key_code_from_mission_control_space_number=(18 19 20 21 23 22 26 28 25 29 122 120 99 118 96 97)
-  
-  key_code_for_requested_mission_control_space=${key_code_from_mission_control_space_number[$number_of_space_to_which_to_move]}
-
-  osascript - "$key_code_for_requested_mission_control_space" <<'APPLESCRIPT'
-  on run argv
-    set key_code_for_requested_mission_control_space to (item 1 of argv) as integer
-    tell application "System Events"
-      key code key_code_for_requested_mission_control_space using {control down, option down, command down}
-    end tell
-  end run
-APPLESCRIPT
-
-  sleep "$sleeptime_to_allow_for_navigation_to_new_space"
-  report_end_phase "Leaving move_to_mission_control_space_n : $*"
-}
-
-function validate_number_of_mission_control_space() {
-  # Fatally errors if supplied number is not a valid number for a Mission Control Space.
-  report_start_phase_standard
-  local space_number="${1:?missing number of Mission Control Space}"
-
-  if (( space_number < 1 ||
-        space_number > MAXIMUM_NUMBER_OF_MISSION_CONTROL_SPACES )); then
-    report_fail "Invalid Mission Control Space number: ${space_number}"
-    return 1
-  fi
-  
-  report_end_phase_standard
-}
-
-function get_wallpaper_container_path_for_space_number() {
-  # Returns path to item (file or directory) in wallpaper directory that is or immediately
-  # contains the wallpaper for the given Space number.
-  #
-  # The “container” for a wallpaper is either:
-  # - the wallpaper file itself, if it is at the root of USER_WALLPAPER_DIRECTORY
-  # - the immediate subdirectory of USER_WALLPAPER_DIRECTORY that immediately contains the wallpaper
-  # 
-  # The name of the container is encoded with a prefix of the form '1_' or '12_', followed by a string
-  # that is converted to the name of the associated Space (by replacing any embedded underscores with spaces).
-
-  report_start_phase_standard
-
-  local -i space_number="${1:?missing space number}"
-  validate_number_of_mission_control_space
-
-  # HINT: USER_WALLPAPER_DIRECTORY="${LOCAL_DROPBOX_DIRECTORY}/Users/${USER}/Prefs/Mission_Control_wallpapers"
-  local user_wallpaper_directory
-  user_wallpaper_directory="$USER_WALLPAPER_DIRECTORY
-  if [[ ! -d "$user_wallpaper_directory" ]]; then
-    report_fail "Wallpaper directory does not exist: ${user_wallpaper_directory}"
-    return 1
-  fi
-
-  # Find the unique matching file or directory that has the prefix "${space_number}_"
-  local -a matching_items
-  matching_items=("${user_wallpaper_directory}/${space_number}_"*(Non))
-  if (( ${#matching_items} == 0 )); then
-    report_fail "No wallpaper file or directory was found for Mission Control Space ${space_number} in: ${user_wallpaper_directory}"
-    return 1
-  fi
-
-  local path_of_wallpaper_container
-  path_of_wallpaper_container="${matching_items[1]}"
-  print -r -- "$path_of_wallpaper_container"
-  
-  report_end_phase_standard
-}
-
-function get_path_to_wallpaper_for_mission_control_space_n() {
+function get_path_to_wallpaper_for_mission_control_space_by_number() {
   # Prints to stdout the path of the wallpaper assigned to the supplied Mission
   # Control Space, referenced by number 1–16.
   #
@@ -245,7 +131,7 @@ function get_path_to_wallpaper_for_mission_control_space_n() {
   report_start_phase_standard
 
   local -i space_number="${1:?missing space number}"
-  validate_number_of_mission_control_space
+  validate_number_of_mission_control_space "$space_number"
 
   local path_of_wallpaper
   local candidate_path
@@ -287,6 +173,118 @@ function get_path_to_wallpaper_for_mission_control_space_n() {
 
   print -r -- "$path_of_wallpaper"
   report_end_phase_standard
+}
+
+function get_wallpaper_container_path_for_space_number() {
+  # Returns path to item (file or directory) in wallpaper directory that is or immediately
+  # contains the wallpaper for the given Space number.
+  #
+  # The “container” for a wallpaper is either:
+  # - the wallpaper file itself, if it is at the root of USER_WALLPAPER_DIRECTORY
+  # - the immediate subdirectory of USER_WALLPAPER_DIRECTORY that immediately contains the wallpaper
+  # 
+  # The name of the container is encoded with a prefix of the form '1_' or '12_', followed by a string
+  # that is converted to the name of the associated Space (by replacing any embedded underscores with spaces).
+
+  report_start_phase_standard
+
+  local -i space_number="${1:?missing space number}"
+  validate_number_of_mission_control_space "$space_number"
+
+  # HINT: USER_WALLPAPER_DIRECTORY="${LOCAL_DROPBOX_DIRECTORY}/Users/${USER}/Prefs/Mission_Control_wallpapers"
+  local user_wallpaper_directory
+  user_wallpaper_directory="$USER_WALLPAPER_DIRECTORY"
+  if [[ ! -d "$user_wallpaper_directory" ]]; then
+    report_fail "Wallpaper directory does not exist: ${user_wallpaper_directory}"
+    return 1
+  fi
+
+  # Find the unique matching file or directory that has the prefix "${space_number}_"
+  local -a matching_items
+  matching_items=("${user_wallpaper_directory}/${space_number}_"*(Non))
+  if (( ${#matching_items} == 0 )); then
+    report_fail "No wallpaper file or directory was found for Mission Control Space ${space_number} in: ${user_wallpaper_directory}"
+    return 1
+  fi
+
+  local path_of_wallpaper_container
+  path_of_wallpaper_container="${matching_items[1]}"
+  print -r -- "$path_of_wallpaper_container"
+  
+  report_end_phase_standard
+}
+
+function validate_number_of_mission_control_space() {
+  # Fatally errors if supplied number is not a valid number for a Mission Control Space.
+  report_start_phase_standard
+  local space_number="${1:?missing number of Mission Control Space}"
+
+  if (( space_number < 1 ||
+        space_number > MAXIMUM_NUMBER_OF_MISSION_CONTROL_SPACES )); then
+    report_fail "Invalid Mission Control Space number: ${space_number}"
+    return 1
+  fi
+  
+  report_end_phase_standard
+}
+
+function set_all_displays_of_current_mission_control_space_to_image_at_path() {
+  # Sets wallpaper of all displays of current Mission Control Space to image at supplied path.
+  report_start_phase_standard
+  local wallpaper_path="${1:?MISSING wallpaper_path}"
+
+  local sleeptime_to_allow_deployment_of_wallpapers_to_all_desktop_in_given_space=1
+
+  report_to_log "Current wallpaper path:${NEWLINE}${wallpaper_path}"
+
+  osascript - "$wallpaper_path" <<'APPLESCRIPT'
+on run argv
+  set wallpaper_path to item 1 of argv
+  tell application "System Events"
+    tell every desktop to set picture to wallpaper_path
+  end tell
+end run
+APPLESCRIPT
+
+  sleep "$sleeptime_to_allow_deployment_of_wallpapers_to_all_desktop_in_given_space"
+  report_end_phase_standard
+}
+
+function move_to_mission_control_space_by_number() {
+  # Moves to desired Mission Control Space, specified by its number 1–16
+  #
+  # Relies on assumed already-implemented Hotkey assignments:
+  #  1: ⌃⌥⌘1
+  #  2: ⌃⌥⌘2
+  #  …
+  #  9: ⌃⌥⌘9
+  # 10: ⌃⌥⌘0
+  # 11: ⌃⌥⌘F1
+  # …
+  # 16: ⌃⌥⌘F6
+  
+  report_start_phase "Entering move_to_mission_control_space_by_number : $*"
+  
+  local sleeptime_to_allow_for_navigation_to_new_space=1
+  local -i number_of_space_to_which_to_move
+  number_of_space_to_which_to_move="${1:?MISSING number of space to move to}"
+
+  local key_code_for_requested_mission_control_space
+  local -a key_code_from_mission_control_space_number=(18 19 20 21 23 22 26 28 25 29 122 120 99 118 96 97)
+  
+  key_code_for_requested_mission_control_space=${key_code_from_mission_control_space_number[$number_of_space_to_which_to_move]}
+
+  osascript - "$key_code_for_requested_mission_control_space" <<'APPLESCRIPT'
+  on run argv
+    set key_code_for_requested_mission_control_space to (item 1 of argv) as integer
+    tell application "System Events"
+      key code key_code_for_requested_mission_control_space using {control down, option down, command down}
+    end tell
+  end run
+APPLESCRIPT
+
+  sleep "$sleeptime_to_allow_for_navigation_to_new_space"
+  report_end_phase "Leaving move_to_mission_control_space_by_number : $*"
 }
 
 function extension_is_valid_wallpaper_image_type() {
