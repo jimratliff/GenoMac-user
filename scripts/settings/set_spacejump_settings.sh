1713f5756fbd2d719ae527155a470d3e2b9d744b
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-01 19:02:04 -0700

    Update set_spacejump_settings.sh

--- scripts/settings/set_spacejump_settings.sh
+++ scripts/settings/set_spacejump_settings.sh
@@ -3,7 +3,7 @@
 function conditionally_configure_spacejump() {
   report_start_phase_standard
 
-  if test_genomac_user_state "$SESH_SPACEJUMP_USER_WANTS_IT"; then
+  if ! test_genomac_user_state "$SESH_SPACEJUMP_USER_WANTS_IT"; then
     report "Skipping configuring SpaceJump because this user doesn’t want it."
     report_end_phase_standard
     return 0
