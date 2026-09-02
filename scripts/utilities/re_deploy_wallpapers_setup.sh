9898cd7899388b082536fce067e8b8b345c103dc
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-01 20:07:23 -0700

    Update re_deploy_wallpapers_setup.sh

--- scripts/utilities/re_deploy_wallpapers_setup.sh
+++ scripts/utilities/re_deploy_wallpapers_setup.sh
@@ -5,10 +5,12 @@ set -euo pipefail
 
 source "${HOME}/.genomac-user/scripts/0_initialize_me_first.sh"
 
+safe_source "${GMU_SETTINGS_SCRIPTS}/set_spacejump_settings.sh"
 safe_source "${GMU_SETTINGS_SCRIPTS}/set_wallpapers.sh"
 
 function main() {
   set_wallpapers_for_all_spaces
+  specify_Space_names_in_SpaceJump
 }
 
 main
