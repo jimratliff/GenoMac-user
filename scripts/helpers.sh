b58a7ef54daa3c17bf967087a61647de7234eb3e
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-04 19:51:20 -0700

    Update helpers.sh

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,32 +1,5 @@
 #!/usr/bin/env zsh
 
-function file_exists_and_is_readable() {
-  # Tests supplied file for (a) existence and, if so, (b) whether it is a readable regular file.
-  # Returns 0 if both exists and readable/regular.
-  # Returns 1 if the file doesn’t exist (a non-error, normal outcome).
-  # Exits immediately as an error if the file exists but isn’t readable/regular.
-  
-  report_start_phase_standard
-  
-  local filepath="${1:?missing file path}"
-
-  if [[ ! -e "${filepath}" && ! -L "${filepath}" ]]; then
-    report_to_log "No file exists at “${filepath}”."
-    report_end_phase_standard
-    return 1
-  elif [[
-    ! -f "${filepath}" ||
-    ! -r "${filepath}"
-  ]]; then
-    report_fail "The object at “${filepath}” isn’t a readable regular file."
-    exit 1
-  else
-    report_to_log "There is a readable regular file at “${filepath}”."
-    report_end_phase_standard
-    return 0
-  fi
-}
-
 
 ############### BELOW THIS POINT IS DEPRECATED ###############
 
