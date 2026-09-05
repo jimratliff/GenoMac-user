4c03b913bde2d388640c60c1e9c3be068dd5e0f6
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-05 16:56:37 -0700

    Update assign_user_environment_variables.sh

--- scripts/assign_user_environment_variables.sh
+++ scripts/assign_user_environment_variables.sh
@@ -172,9 +172,9 @@ META_MODIFIER_CHARS="${CONTROL_CHAR}${OPTION_CHAR}${COMMAND_CHAR}"
 MODIFIERS_KEYBOARD_NAVIGATION="${SHIFT_CHAR}${OPTION_CHAR}${COMMAND_CHAR}"
 
 FINDER_SIDEBAR_FAVORITES_BAREBONES=(
-  "[\"🪚 Applications\", \"/Applications\"]"
-  "[\"🛠️ Utilities\", \"/Applications/Utilities\"]"
-  "[\"🏠 $USER\", \"~\"]"
+  "[\"Applications\", \"/Applications\"]"
+  "[\"Utilities\", \"/Applications/Utilities\"]"
+  "[\"$USER\", \"~\"]"
   "[\"Library\", \"~/Library\"]"
 )
 
