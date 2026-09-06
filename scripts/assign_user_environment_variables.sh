e1e3d33d418add5f48e6b69e3a4352e5a5430fa8
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-05 17:09:51 -0700

    Update assign_user_environment_variables.sh

--- scripts/assign_user_environment_variables.sh
+++ scripts/assign_user_environment_variables.sh
@@ -176,6 +176,7 @@ FINDER_SIDEBAR_FAVORITES_BAREBONES=(
   "[\"Utilities\", \"/Applications/Utilities\"]"
   "[\"$USER\", \"~\"]"
   "[\"Library\", \"~/Library\"]"
+  "[\"Nonexistent file\", \"~/Library/nope\"]"
 )
 
 ############### States related to features of macOS
