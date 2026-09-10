1be554a320edde11717b54eac8e5f9a3e8089b9d
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-09 21:38:01 -0700

    Update make_directory_for_Dock_aliases.sh

--- scripts/installations/make_directory_for_Dock_aliases.sh
+++ scripts/installations/make_directory_for_Dock_aliases.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env zsh
 
+function conditionally_create_directory_for_aliases_for_Dock() {
+  # Conditionally create directory of aliases, which directory will reside in the Dock.
+  report_start_phase_standard
+
+  run_if_user_has_not_done \
+    "$SESH_ALIAS_DIRECTORY_HAS_BEEN_CREATED" \
+    create_directory_for_aliases_for_Dock \
+    "Skipping creating directory for Finder alias files, because this was done earlier this session."
+    
+  report_end_phase_standard
+}
+
 function create_directory_for_aliases_for_Dock() {
   # Creates directory of aliases, which directory will reside in the Dock.
   # Adds a README explaining purpose of this directory.
   #
-  # Executes unconditionally, i.e., even if this directory has been created previously.
+  # Executes every session, i.e., even if this directory has been created during a previous session.
+  # This wasy it heals either a deletion of the directory or a screw up in the permissions.
 
   report_start_phase_standard
 
