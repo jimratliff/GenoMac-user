6d3c5d4549260d9ce8080e4c7240147c6e2a1f61
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-11 11:15:13 -0700

    Update make_directory_for_Dock_aliases.sh

--- scripts/installations/make_directory_for_Dock_aliases.sh
+++ scripts/installations/make_directory_for_Dock_aliases.sh
@@ -3,9 +3,12 @@
 function create_directory_for_aliases_for_Dock() {
   # Creates directory of aliases, which directory will reside in the Dock.
   # Adds a README explaining purpose of this directory.
+  #
+  # HINT: DIRECTORY_OF_ALIASES_FOR_DOCK="$HOME/Documents/Aliases_for_Dock"
 
   report_start_phase_standard
 
+  # HINT: GMU_FILES_TO_COPY_ELSEWHERE="${GMU_RESOURCES}/files_to_be_copied_elsewhere"
   local readme_file_path="${GMU_FILES_TO_COPY_ELSEWHERE}/0_README_aliases_for_Dock.md"
 
   report_action_taken "Create directory for Finder alias files to be referenced from the Dock: “${DIRECTORY_OF_ALIASES_FOR_DOCK}”"
