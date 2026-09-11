bbdd78ba5a4e170866d061f3f93030908305e3f1
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-11 15:24:29 -0700

    Update assign_user_environment_variables.sh

--- scripts/assign_user_environment_variables.sh
+++ scripts/assign_user_environment_variables.sh
@@ -189,7 +189,6 @@ FINDER_SIDEBAR_FAVORITES_BAREBONES=(
   "[\"Utilities\", \"/Applications/Utilities\"]"
   "[\"$USER\", \"~\"]"
   "[\"Library\", \"~/Library\"]"
-  "[\"♟️ Chess\", \"~/Documents/HIARCS Chess\"]"
 )
 
 ############### States related to features of macOS
