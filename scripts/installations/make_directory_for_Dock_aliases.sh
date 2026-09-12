369ebe7e1d327cd19d7ba365e30df79bfbbbfa22
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-09-12 10:08:42 -0700

    Update make_directory_for_Dock_aliases.sh

--- scripts/installations/make_directory_for_Dock_aliases.sh
+++ scripts/installations/make_directory_for_Dock_aliases.sh
@@ -11,14 +11,14 @@ function create_directory_for_aliases_for_Dock() {
   # HINT: GMU_FILES_TO_COPY_ELSEWHERE="${GMU_RESOURCES}/files_to_be_copied_elsewhere"
   local readme_file_path="${GMU_FILES_TO_COPY_ELSEWHERE}/0_README_aliases_for_Dock.md"
 
-  report_action_taken "Create directory for Finder alias files to be referenced from the Dock: “${DIRECTORY_OF_ALIASES_FOR_DOCK}”"
-  mkdir -p "$DIRECTORY_OF_ALIASES_FOR_DOCK" ; success_or_not
+  report_action_taken_to_log "Create directory for Finder alias files to be referenced from the Dock: “${DIRECTORY_OF_ALIASES_FOR_DOCK}”"
+  mkdir -p "$DIRECTORY_OF_ALIASES_FOR_DOCK"
   
-  report_adjust_setting "Set permissions on local aliases-for-Dock directory"
-  chmod 700 "$DIRECTORY_OF_ALIASES_FOR_DOCK" ; success_or_not
-
-  report_action_taken "Copy README to aliases-for-Dock directory, replacing any existing version"
-  cp -f "$readme_file_path" "$DIRECTORY_OF_ALIASES_FOR_DOCK/" ; success_or_not
+  report_action_taken_to_log "Set permissions on local aliases-for-Dock directory"
+  chmod 700 "$DIRECTORY_OF_ALIASES_FOR_DOCK"
+  
+  report_action_taken_to_log "Copy README to aliases-for-Dock directory, replacing any existing version"
+  cp -f "$readme_file_path" "$DIRECTORY_OF_ALIASES_FOR_DOCK/"
   
   report_end_phase_standard
 }
